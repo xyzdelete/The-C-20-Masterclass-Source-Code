6c67fee36436fd30efa2d6a637c7952460e64a09
Arturs Anikins <xyzdelete@protonmail.com>
2023-07-11 10:40:31 +0300

    33. Errors

--- 3.FirstSteps/3.4ErrorsWarnings/main.cpp
+++ 3.FirstSteps/3.4ErrorsWarnings/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
-int main(){
-   
-   //Compile time error
-   std::cout << "Hello Word!" << std::endl;
+int main()
+{
+    // Compile time error
+    std::cout << "Hello Word!" << std::endl;
 
-   //Run time error
-   int value = 7/0;
-   std::cout << "value : " << value << std::endl;
-
+    // Run time error
+    int  value = 7 / 0;
+    std::cout << "value: " << value << std::endl;
     return 0;
 }
