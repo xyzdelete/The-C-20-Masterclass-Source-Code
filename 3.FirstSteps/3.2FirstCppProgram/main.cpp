77daffd021332b492cdb0a33e24ed888392d06a1
Arturs Anikins <xyzdelete@protonmail.com>
2023-07-11 10:18:48 +0300

    31. Your First C++ Program

--- 3.FirstSteps/3.2FirstCppProgram/main.cpp
+++ 3.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
-int main(){
-
-    std::cout << "Number1" << std::endl;
-    std::cout << "Number2" << std::endl;
-    std::cout << "Number3" << std::endl;
+int main()
+{
+    std::cout << "Arturs1" << std::endl;
+    std::cout << "Arturs2" << std::endl;
+    std::cout << "Arturs3" << std::endl;
+    std::cout << "Arturs4" << std::endl;
+    std::cout << "Arturs5" << std::endl;
+    std::cout << "Arturs6" << std::endl;
+    std::cout << "Arturs7" << std::endl;
+    std::cout << "Arturs8" << std::endl;
+    std::cout << "Arturs9" << std::endl;
+    std::cout << "Arturs10" << std::endl;
 
     return 0;
 }
