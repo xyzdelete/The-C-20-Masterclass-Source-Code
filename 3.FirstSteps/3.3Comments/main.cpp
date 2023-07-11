8d7f68498f9ddf802cca5aca9c42dcd209f84c1a
Arturs Anikins <xyzdelete@protonmail.com>
2023-07-11 10:26:46 +0300

    32. Comments

--- 3.FirstSteps/3.3Comments/main.cpp
+++ 3.FirstSteps/3.3Comments/main.cpp
@@ -1,19 +1,8 @@
-//One line comment
-
-//This brings in the iostream library
+// This brings in the iostream library
 #include <iostream>
 
-/*
-    This is a block comment that englobes multiple
-    lines of text
-*/
-
-
-
-
-int main(){
-    //This is going to print "Hello World" to the console
+int main()
+{
     std::cout << "Hello World!" << std::endl;
     return 0;
-    //Program ends here
 }
