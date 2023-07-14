62882f7205d280526eee8ad79ce021f77579de2d
Arturs Anikins <xyzdelete@protonmail.com>
2023-07-14 11:32:02 +0300

    44. Characters And Text

--- 4.VariablesAndDatatypes/4.7CharactersAndText/main.cpp
+++ 4.VariablesAndDatatypes/4.7CharactersAndText/main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
+int main()
+{
+    char character1{'a'};
+    char character2{'r'};
+    char character3{'r'};
+    char character4{'o'};
+    char character5{'w'};
 
-int main(){
-
-	char character1 {'a'};
-    char character2 {'r'};
-    char character3 {'r'};
-    char character4 {'o'};
-    char character5 {'w'};
-    
     std::cout << character1 << std::endl;
     std::cout << character2 << std::endl;
     std::cout << character3 << std::endl;
@@ -17,10 +16,10 @@ int main(){
 
     //One byte in memory : 2^8 = 256 different values (0 ~ 255)
     std::cout << std::endl;
-    
-    char value = 65 ; // ASCII character code for 'A'
+
+    char value = 65; // ASCII character code for 'A'
     std::cout << "value : " << value << std::endl; // A
-    std::cout << "value(int) : " << static_cast<int>(value) << std::endl; 
+    std::cout << "value(int) : " << static_cast<int>(value) << std::endl;
 
     return 0;
 }
