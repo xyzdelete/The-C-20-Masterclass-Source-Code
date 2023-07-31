5f4cd162c8f2c0f5a73a98c4e33d6f86a13e9368
Arturs Anikins <xyzdelete@protonmail.com>
2023-07-31 12:51:31 +0300

    90. If with Initializer

--- 10.FlowControl/10.9IfWithInitializer/main.cpp
+++ 10.FlowControl/10.9IfWithInitializer/main.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 
+int main()
+{
+	bool go { true };
 
+	if constexpr (auto speed { 10 }, speed2 { 2 }; false)
+	{
+		std::cout << "speed : " << speed << std::endl;
 
-int main(){
-
-	
-	bool go {true};
-
-	if(int speed {10};go){
-        std::cout << "speed : " << speed << std::endl;
-		
-		if(speed > 5){
+		if (speed > 5)
+		{
 			std::cout << "Slow down!" << std::endl;
-		}else{
+		}
+		else
+		{
 			std::cout << "All good!" << std::endl;
 		}
-	}else{
-        std::cout << "speed : " << speed << std::endl;
+	}
+	else
+	{
+		std::cout << "speed : " << speed << std::endl;
+		std::cout << "speed : " << speed2 << std::endl;
 		std::cout << "Stop" << std::endl;
 	}
 
-    //std::cout << "Out of the if block , speed : " << speed << std::endl;
-   
-    return 0;
+	//std::cout << "Out of the if block , speed : " << speed << std::endl;
+
+	return 0;
 }
