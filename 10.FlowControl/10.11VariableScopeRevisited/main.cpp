f758cd16a1d1746d17efa5826ace23fcfd597007
Arturs Anikins <xyzdelete@protonmail.com>
2023-07-31 13:07:54 +0300

    92. Variable Scope Revisited

--- 10.FlowControl/10.11VariableScopeRevisited/main.cpp
+++ 10.FlowControl/10.11VariableScopeRevisited/main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 
-int global_var{44};
+int global_var { 44 };
 
-int main(){
-
-    bool green {false};
+int main()
+{
+    bool green { false };
 
     global_var++; // 45
 
-    if(green){
-        global_var++; 
-        int car_count{23};
+    if (green)
+    {
+        global_var++;
+        int car_count { 23 };
         std::cout << "Light is green " << car_count << "on the move!" << std::endl;
-    }else{
+    }
+    else
+    {
         //car_count++; // Compiler error
         global_var++; // 46
         std::cout << "Light is not green. Y'all should STOP!" << std::endl;
@@ -20,6 +23,6 @@ int main(){
 
     //car_count++;
     std::cout << "global_var : " << global_var << std::endl;
-   
+
     return 0;
 }
