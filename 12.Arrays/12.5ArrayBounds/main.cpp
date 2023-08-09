d6aa9fc8d1e33c315484100e1ce421cd0b78da69
Arturs Anikins <xyzdelete@protonmail.com>
2023-08-09 13:00:29 +0300

    115. Array Bounds

--- 12.Arrays/12.5ArrayBounds/main.cpp
+++ 12.Arrays/12.5ArrayBounds/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
+int main()
+{
+	int numbers[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
 
-int main(){
-
-	int numbers[] {1,2,3,4,5,6,7,8,9,0};
-	
 	//Read beyond bounds : Will read garbage or crash your program
 	//std::cout << "numbers[12] : " << numbers[12] << std::endl;
 
@@ -16,8 +15,8 @@ int main(){
 	numbers[129] = 1000;
 	std::cout << "numbers[129] : " << numbers[129] << std::endl;
 
-    std::cout << "Program ending...." << std::endl;
+	std::cout << "Program ending...." << std::endl;
 
-    
-    return 0;
+
+	return 0;
 }
