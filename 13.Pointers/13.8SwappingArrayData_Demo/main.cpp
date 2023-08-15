a747087a8824e4579340b5cd5e2aaedf61305855
Arturs Anikins <xyzdelete@protonmail.com>
2023-08-15 09:50:56 +0300

    129. Swapping array data : A demo

--- 13.Pointers/13.8SwappingArrayData_Demo/main.cpp
+++ 13.Pointers/13.8SwappingArrayData_Demo/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 
-
-int main(){
-
-    int arr0[5]{ 1,2,3,4,5 };
-    int arr1[5]{ 6,7,8,9,10 };
-    int * p_arr1 {arr1};
-    int * p_arr0 {arr0};
+int main()
+{
+    int arr0[5] { 1, 2, 3, 4, 5 };
+    int arr1[5] { 6, 7, 8, 9, 10 };
+    int* p_arr1 { arr1 };
+    int* p_arr0 { arr0 };
 
     //Print arr0
     std::cout << "arr0 : ";
-    for (size_t i{}; i < std::size(arr0); ++i) {
+    for (size_t i {}; i < std::size(arr0); ++i)
+    {
         std::cout << p_arr0[i] << " ";
     }
     std::cout << std::endl;
 
     //Print arr1
     std::cout << "arr1 : ";
-    for (size_t i{}; i < std::size(arr1); ++i) {
+    for (size_t i {}; i < std::size(arr1); ++i)
+    {
         std::cout << p_arr1[i] << " ";
     }
     std::cout << std::endl;
@@ -42,14 +43,14 @@ int main(){
     }
     */
 
-   /*
-    int * temp{nullptr};
+    /*
+     int * temp{nullptr};
 
-    temp = arr1;
-    arr1 = arr0;
-    */
+     temp = arr1;
+     arr1 = arr0;
+     */
 
-    int * temp{nullptr};
+    int* temp { nullptr };
 
     temp = p_arr1;
     p_arr1 = p_arr0;
@@ -57,20 +58,22 @@ int main(){
 
     //Print arr0
     std::cout << "arr0 : ";
-    for (size_t i{}; i < std::size(arr0); ++i) {
+    for (size_t i {}; i < std::size(arr0); ++i)
+    {
         std::cout << p_arr0[i] << " ";
     }
     std::cout << std::endl;
 
     //Print arr1
     std::cout << "arr1 : ";
-    for (size_t i{}; i < std::size(arr1); ++i) {
+    for (size_t i {}; i < std::size(arr1); ++i)
+    {
         std::cout << p_arr1[i] << " ";
     }
     std::cout << std::endl;
 
 
 
-  
+
     return 0;
 }
