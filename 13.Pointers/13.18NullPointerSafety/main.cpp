cee1cc9864599347355743c73d7ed9c46b667e6c
Arturs Anikins <xyzdelete@protonmail.com>
2023-08-17 11:48:57 +0300

    139. Null pointer safety

--- 13.Pointers/13.18NullPointerSafety/main.cpp
+++ 13.Pointers/13.18NullPointerSafety/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-int main(){
+int main()
+{
+    //Verbose nullptr check
+    std::cout << std::endl;
+    std::cout << "Verbose nullptr check: " << std::endl;
 
-	//Verbose nullptr check
-	std::cout << std::endl;
-	std::cout << "Verbose nullptr check: " << std::endl;
-	
-    int *p_number{};//Initialized to nullptr
+    int* p_number {};//Initialized to nullptr
     //p_number = new int(7);
-    
+
     /*
     if(!(p_number==nullptr)){
         std::cout << "p_number points to a VALID address : "<< p_number << std::endl;
@@ -17,10 +17,13 @@ int main(){
         std::cout << "p_number points to an INVALID address." << std::endl;
     }
     */
-    if(p_number){
-        std::cout << "p_number points to a VALID address : "<< p_number << std::endl;
+    if (p_number)
+    {
+        std::cout << "p_number points to a VALID address : " << p_number << std::endl;
         std::cout << "*p_number : " << *p_number << std::endl;
-    }else{
+    }
+    else
+    {
         std::cout << "p_number points to an INVALID address." << std::endl;
     }
 
@@ -28,15 +31,15 @@ int main(){
     nullptr;
 
     //It is OK call delete on a nullptr
-	//Calling delete on a nullptr is OK
-    int *p_number1 {};
-    
-    delete p_number1;	// This won't cause any problem
-						//if p_number1 contains nullptr
+    //Calling delete on a nullptr is OK
+    int* p_number1 {};
 
-    // if(p_number1 != nullptr){
-    //     delete p_number1;
-    // }
-   
+    delete p_number1;	// This won't cause any problem
+    //if p_number1 contains nullptr
+
+// if(p_number1 != nullptr){
+//     delete p_number1;
+// }
+
     return 0;
 }
