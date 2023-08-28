f21b369cc90ed2708ffb4aacd6c1bd557df3d0f9
Arturs Anikins <xyzdelete@protonmail.com>
2023-08-28 08:48:01 +0300

    144. Declaring and using references

--- 14.References/14.2.DeclaringAndUsingReferences/main.cpp
+++ 14.References/14.2.DeclaringAndUsingReferences/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-int main(){
-
-    int int_data{33};
-    double double_data{55};
+int main()
+{
+    int int_data { 33 };
+    double double_data { 55 };
 
     //References
-    int& ref_int_data{int_data};
-    double& ref_double_data{double_data};
+    int& ref_int_data { int_data };
+    double& ref_double_data { double_data };
 
     //Print stuff out
     std::cout << "int_data : " << int_data << std::endl;
@@ -45,7 +45,7 @@ int main(){
     ref_double_data = 1000.45;
 
 
-   //Print stuff out
+    //Print stuff out
     std::cout << std::endl;
     std::cout << "int_data : " << int_data << std::endl;
     std::cout << "&int_data : " << &int_data << std::endl;
@@ -58,6 +58,6 @@ int main(){
     std::cout << "&ref_int_data : " << &ref_int_data << std::endl;
     std::cout << "ref_double_data : " << ref_double_data << std::endl;
     std::cout << "&ref_double_data : " << &ref_double_data << std::endl;
-    
+
     return 0;
 }
