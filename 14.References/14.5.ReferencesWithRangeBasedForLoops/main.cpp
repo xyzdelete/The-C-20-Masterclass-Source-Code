16a1d841528c4d817fa0a5101abce7f70ab0ef3e
Arturs Anikins <xyzdelete@protonmail.com>
2023-08-28 09:24:18 +0300

    147. References with Range based for loops

--- 14.References/14.5.ReferencesWithRangeBasedForLoops/main.cpp
+++ 14.References/14.5.ReferencesWithRangeBasedForLoops/main.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 
 
-int main(){
+int main()
+{
+    int scores[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    int scores[] {1,2,3,4,5,6,7,8,9,10};
-    
     //Printing positions
     std::cout << std::endl;
-	
-	std::cout << "Scores : ";
-    for ( auto score : scores){
-        std::cout << " " << score ;
+
+    std::cout << "Scores : ";
+    for (auto score : scores)
+    {
+        std::cout << " " << score;
     }
-	std::cout << std::endl;
+    std::cout << std::endl;
 
 
-   for ( auto& score : scores){
+    for (auto& score : scores)
+    {
         score = score * 10;
     }
 
     //Printing after change
-     std::cout << std::endl;
-	std::cout << "Scores : ";
-    for ( auto score : scores){
-        std::cout << " " << score ;
+    std::cout << std::endl;
+    std::cout << "Scores : ";
+    for (auto score : scores)
+    {
+        std::cout << " " << score;
     }
-	std::cout << std::endl;
-    
+    std::cout << std::endl;
+
     return 0;
 }
