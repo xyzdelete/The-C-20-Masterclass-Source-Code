0490890d7e40e1ab88e7b493b4efcde91f8c6310
Arturs <xyzdelete@protonmail.com>
2023-09-05 09:50:58 +0300

    172. Multiple Files : Revisition the Linker stage

--- 16.Functions/16.4MultipleFiles_CompilationModelRevisited/main.cpp
+++ 16.Functions/16.4MultipleFiles_CompilationModelRevisited/main.cpp
@@ -2,21 +2,20 @@
 #include "compare.h" // Preprocessor
 #include "operations.h"
 
-
-int main(){
-
-    int maximum = max(134,56);
+int main()
+{
+    int maximum = max(134, 56);
     std::cout << "max : " << maximum << std::endl;
 
-    int minimum = min(146,23);
+    int minimum = min(146, 23);
     std::cout << "min : " << minimum << std::endl;
 
-    int x{4}; // 5
-    int y{5}; // 6
+    int x { 4 }; // 5
+    int y { 5 }; // 6
 
-    int result = incr_mult(x,y);
+    int result = incr_mult(x, y);
     std::cout << "result : " << result << std::endl;
-   
+
     return 0;
 }
 
