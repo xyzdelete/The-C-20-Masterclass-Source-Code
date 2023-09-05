6784851d27163fc1ad131476bdb390df29b8954b
Arturs <xyzdelete@protonmail.com>
2023-09-05 12:02:14 +0300

    174. Pass by const value

--- 16.Functions/16.6PassByConstValue/main.cpp
+++ 16.Functions/16.6PassByConstValue/main.cpp
@@ -2,18 +2,19 @@
 
 void say_age(const int age); // Declaration
 
-int main(){
-
-    int age{23}; // Local
+int main()
+{
+    int age { 23 }; // Local
     std::cout << "age (before call) : " << age << "&age : " << &age << std::endl;
     say_age(age); // Argument
-    std::cout << "age (after call) : " << age << "&age : " << &age <<  std::endl;
+    std::cout << "age (after call) : " << age << "&age : " << &age << std::endl;
 
     return 0;
 }
 
 
-void say_age(const int age){ // Parameter
-    ///++age; 
-    std::cout <<  "Hello , you are " << age << " years old! &age : " << &age <<  std::endl;
+void say_age(const int age)
+{ // Parameter
+///++age; 
+    std::cout << "Hello , you are " << age << " years old! &age : " << &age << std::endl;
 }
