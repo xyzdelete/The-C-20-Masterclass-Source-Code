27660dd6573b6ce00e2101a4527ac1a77469d7f8
Arturs <xyzdelete@protonmail.com>
2023-09-06 10:09:37 +0300

    182. Sized array function parameters

--- 16.Functions/16.14SizedArrayFunctionParameters/main.cpp
+++ 16.Functions/16.14SizedArrayFunctionParameters/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 
 
-double sum ( double scores[], size_t count){
-    double sum{};
-        
-    for(size_t i{} ; i < count ; ++i){
-       sum += scores[i];
+double sum(double scores[], size_t count)
+{
+    double sum {};
+
+    for (size_t i {} ; i < count ; ++i)
+    {
+        sum += scores[i];
     }
     return sum;
 }
 
 
-int main(){
+int main()
+{
 
-    double student_scores[] {10.0,20.0,30.0,4,5,6,7,8,9}; // Less than 5 parameters
+    double student_scores[] { 10.0,20.0,30.0,4,5,6,7,8,9 }; // Less than 5 parameters
 
-    double result = sum(student_scores,std::size(student_scores));
+    double result = sum(student_scores, std::size(student_scores));
     std::cout << "result : " << result << std::endl;
-   
+
     return 0;
 }
