9e14618db943329eb1d194e56e8737b662a59c1a
Arturs <xyzdelete@protonmail.com>
2023-09-07 09:57:05 +0300

    189. string_view Parameters

--- 16.Functions/16.21String_viewParameters/main.cpp
+++ 16.Functions/16.21String_viewParameters/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <string_view>
 
-void say_my_name(std::string_view name){
+void say_my_name(std::string_view name)
+{
     std::cout << "Hello your name is : " << name << std::endl;
 }
 
-int main(){
+int main()
+{
 
-    std::string some_name{"John"};
+    std::string some_name { "John" };
     //say_my_name("John");
     //say_my_name(some_name);
     say_my_name(std::string_view("Samuel"));
-   
+
     return 0;
 }
