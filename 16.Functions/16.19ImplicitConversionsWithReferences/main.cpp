34d5b34751e683d3c6dae5852b125f8d05b4dc22
Arturs <xyzdelete@protonmail.com>
2023-09-07 09:23:03 +0300

    187. Implicit conversions with references

--- 16.Functions/16.19ImplicitConversionsWithReferences/main.cpp
+++ 16.Functions/16.19ImplicitConversionsWithReferences/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 void increment(int& value);
-void print_out(const int & value);
+void print_out(const int& value);
 
-int main(){
-
+int main()
+{
     /*
     int int_var{3};
 
@@ -12,21 +12,23 @@ int main(){
     print_out(int_var);
     */
 
-   double double_var{3.6};
+    double double_var { 3.6 };
 
-   int data = double_var;
+    int data = double_var;
 
-   increment(data);
-   print_out(double_var);
-    
+    increment(data);
+    print_out(double_var);
+
     return 0;
 }
 
-void increment(int& value){
+void increment(int& value)
+{
     value++;
-	std::cout << "value incremented to : " << value << std::endl;
+    std::cout << "value incremented to : " << value << std::endl;
 }
 
-void print_out(const int & value){
+void print_out(const int& value)
+{
     std::cout << "value : " << value << std::endl;
 }
