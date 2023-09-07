0c4e3fa17700096a2adf8b830ac792507f4a49a6
Arturs <xyzdelete@protonmail.com>
2023-09-07 09:35:43 +0300

    188. Implicit conversions with pointers

--- 16.Functions/16.20ImplicitConversionsWithPointers/main.cpp
+++ 16.Functions/16.20ImplicitConversionsWithPointers/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
-void print_sum(int* param1, int* param2){
-	std::cout << "sum : " << (*param1 + *param2) << std::endl;
+void print_sum(int* param1, int* param2)
+{
+    std::cout << "sum : " << (*param1 + *param2) << std::endl;
 }
 
-int main(){
+int main()
+{
 
-    int a{3};
-    int b{12};
+    int a { 3 };
+    int b { 12 };
 
-    print_sum(&a,&b);
-   
+    print_sum(&a, &b);
+
     return 0;
 }
