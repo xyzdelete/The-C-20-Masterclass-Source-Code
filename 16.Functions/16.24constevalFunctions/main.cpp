91682d859e298f671f6eb21ebf39ba9ea14fb463
Arturs <xyzdelete@protonmail.com>
2023-09-07 11:11:18 +0300

    192. consteval functions

--- 16.Functions/16.24constevalFunctions/main.cpp
+++ 16.Functions/16.24constevalFunctions/main.cpp
@@ -2,16 +2,18 @@
 
 //Evaluate this function at compile time. If you can't 
 //do that throw a compiler error
-consteval int get_value(int multiplier){
+consteval int get_value(int multiplier)
+{
     return 3 * multiplier;
 }
 
-int main(){
+int main()
+{
 
     int result = get_value(4);
 
-    int some_var{5};
+    constexpr int some_var { 5 };
     result = get_value(some_var);
-   
+
     return 0;
 }
