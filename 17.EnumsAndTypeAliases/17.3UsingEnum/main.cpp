11294c489d6133b521550df8551c0712f010f602
Arturs <xyzdelete@protonmail.com>
2023-09-08 12:08:58 +0300

    196. using enum

--- 17.EnumsAndTypeAliases/17.3UsingEnum/main.cpp
+++ 17.EnumsAndTypeAliases/17.3UsingEnum/main.cpp
@@ -2,12 +2,13 @@
 #include <string_view>
 
 enum class Month {
-    Jan , Feb, Mar, Apr ,
+    Jan, Feb, Mar, Apr,
     May, Jun, Jul, Aug,
     Sep, Oct, Nov, Dec
 };
 
-std::string_view month_to_string(Month month){
+std::string_view month_to_string(Month month)
+{
     switch (month)
     {
         /*
@@ -25,29 +26,30 @@ std::string_view month_to_string(Month month){
         case Month::Dec : return "December";
         default : return "None";
         */
-       using enum Month;
-        case Jan : return "January";
-        case Feb : return "February";
-        case Mar : return "March";
-        case Apr : return "April";
-        case May : return "May";
-        case Jun : return "June";
-        case Jul : return "July";
-        case Aug : return "August";
-        case Sep : return "September";
-        case Oct : return "October";
-        case Nov : return "November";
-        case Dec : return "December";
-        default : return "None";
+        using enum Month;
+    case Jan: return "January";
+    case Feb: return "February";
+    case Mar: return "March";
+    case Apr: return "April";
+    case May: return "May";
+    case Jun: return "June";
+    case Jul: return "July";
+    case Aug: return "August";
+    case Sep: return "September";
+    case Oct: return "October";
+    case Nov: return "November";
+    case Dec: return "December";
+    default: return "None";
     }
 
 }
 
 
-int main(){
+int main()
+{
 
-    Month month {Month::Apr};
+    Month month { Month::Apr };
     std::cout << "month : " << month_to_string(month) << std::endl;
-   
+
     return 0;
 }
