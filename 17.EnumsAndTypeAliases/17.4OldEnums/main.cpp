012dc37e8d99a0514489811df1d2fa589e96c34c
Arturs <xyzdelete@protonmail.com>
2023-09-08 12:23:35 +0300

    197. Legacy enumerated types

--- 17.EnumsAndTypeAliases/17.4OldEnums/main.cpp
+++ 17.EnumsAndTypeAliases/17.4OldEnums/main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 
-enum   Direction : unsigned char  {
-    TopLeft = 64, TopRight,Center,BottomLeft,BottomRight
+enum   Direction : unsigned char {
+    TopLeft = 64, TopRight, Center, BottomLeft, BottomRight
 };
-    
+
 enum   Tool {
-    Pen,Marker, Eraser,Rectangle,Circle,PaintBucket
+    Pen, Marker, Eraser, Rectangle, Circle, PaintBucket
 };
 
 
-int main(){
+int main()
+{
 
-	Direction direction{Direction::TopLeft};
-	Tool tool {Tool::Pen};
+    Direction direction { Direction::TopLeft };
+    Tool tool { Tool::Pen };
 
     std::cout << "direction : " << static_cast<unsigned int>(direction) << std::endl;
     std::cout << "tool : " << tool << std::endl;
     //std::cout << "(tool > direction) : " << (tool > direction) << std::endl;
-   
+
     return 0;
 }
