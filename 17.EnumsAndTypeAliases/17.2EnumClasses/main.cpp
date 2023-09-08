f18d647b43e851ceba30c08ad312187be72b5b0b
Arturs <xyzdelete@protonmail.com>
2023-09-08 11:37:48 +0300

    195. Enumerated types (Enum Class)

--- 17.EnumsAndTypeAliases/17.2EnumClasses/main.cpp
+++ 17.EnumsAndTypeAliases/17.2EnumClasses/main.cpp
@@ -3,17 +3,19 @@
 //Declare an enum type
 //The default type associated with enum classes with C++ is int
 //unsigned char : 0 ~ 255
-enum class Month :  char {
-    Jan = -1, January = Jan, Feb, Mar, 
+enum class Month : char
+{
+    Jan = -1, January = Jan, Feb, Mar,
     Apr, May, Jun,
     Jul, Aug, Sep,
     Oct = 100, Nov, Dec
 };
 
 
-int main(){
+int main()
+{
 
-    Month month {Month::Jan};
+    Month month { Month::Jan };
     std::cout << "month : " << static_cast<int>(month) << std::endl;
     std::cout << "sizeof(month) : " << sizeof(month) << std::endl;
 
