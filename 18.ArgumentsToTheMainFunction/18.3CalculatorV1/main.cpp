e0573e86e19fdb306a08e728b690e4e68b44db49
Arturs <xyzdelete@protonmail.com>
2023-09-09 09:34:30 +0300

    202. Calculator V1

--- 18.ArgumentsToTheMainFunction/18.3CalculatorV1/main.cpp
+++ 18.ArgumentsToTheMainFunction/18.3CalculatorV1/main.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 
-int main(){
+int main()
+{
+        double first_number { 5.0 };
+        double second_number { 33.1 };
+        char c { '+' };
 
-	double first_number {5.0};
-    double second_number {33.1};
-    char c{'+'};
-    
-    switch(c){
+        switch (c)
+        {
         case '+':
-        std::cout << first_number << " + " << second_number << " = " 
-                << first_number + second_number << std::endl;
-        break;
-        
+                std::cout << first_number << " + " << second_number << " = "
+                        << first_number + second_number << std::endl;
+                break;
+
         case '-':
-        std::cout << first_number << " - " << second_number << "=" 
-                << first_number - second_number << std::endl;
-        break;
-        
+                std::cout << first_number << " - " << second_number << "="
+                        << first_number - second_number << std::endl;
+                break;
+
         case 'x':
-        std::cout << first_number << " * " << second_number << " = " 
-                << first_number * second_number << std::endl;
-        break;
-        
+                std::cout << first_number << " * " << second_number << " = "
+                        << first_number * second_number << std::endl;
+                break;
+
         case '/':
-        std::cout << first_number << " / " << second_number << " = " 
-                << first_number / second_number << std::endl;
-        break;
-		
-    }
-	return 0;
+                std::cout << first_number << " / " << second_number << " = "
+                        << first_number / second_number << std::endl;
+                break;
+
+        }
+        return 0;
 }
