f9177cc35d2f6cd5454b8f9886af1d41abddc4f4
Arturs <xyzdelete@protonmail.com>
2023-09-10 12:27:05 +0300

    215. Optional output from functions

--- 19.GettingThingsOutOfFuntions/19.11OptionalOutputFromFunctions/main.cpp
+++ 19.GettingThingsOutOfFuntions/19.11OptionalOutputFromFunctions/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
-
-int find_character_v0(const std::string & str, char c){
+int find_character_v0(const std::string& str, char c)
+{
     //If found , return the index, else return -1
-    int not_found {-1};
-    for (size_t i{} ; i < str.size()  ; ++i){
-        if(str.c_str()[i] == c){
+    int not_found { -1 };
+    for (size_t i {} ; i < str.size() ; ++i)
+    {
+        if (str.c_str()[i] == c)
+        {
             return i;
         }
     }
@@ -13,10 +15,13 @@ int find_character_v0(const std::string & str, char c){
 }
 
 
-void find_character_v1(const std::string & str, char c, bool & success){
+void find_character_v1(const std::string& str, char c, bool& success)
+{
     //If found set success to true, else set to false
-    for (size_t i{} ; i < str.size()  ; ++i){
-        if(str.c_str()[i] == c){
+    for (size_t i {} ; i < str.size() ; ++i)
+    {
+        if (str.c_str()[i] == c)
+        {
             success = true;
             return;
         }
@@ -26,10 +31,11 @@ void find_character_v1(const std::string & str, char c, bool & success){
 
 
 
-int main(){
+int main()
+{
 
-	std::string str1 {"Hello World in C++20!"};
-	char c{'r'};
+    std::string str1 { "Hello World in C++20!" };
+    char c { 'r' };
 
 
     /*
@@ -38,19 +44,22 @@ int main(){
     if((find_character_v0(str1,c))!=-1){
         std::cout << "Found character " << c << " at index : " << index << std::endl;
     }else{
-       std::cout << "Didn't find our character" << std::endl; 
+       std::cout << "Didn't find our character" << std::endl;
     }
     */
 
-   bool success{false};
+    bool success { false };
 
-   find_character_v1(str1,c,success);
+    find_character_v1(str1, c, success);
 
-   if(success){
-         std::cout << "Found character " << c << " in our string " <<  std::endl;
-   }else{
-       std::cout << "Didn't find our character" << std::endl;
-   }
-   
+    if (success)
+    {
+        std::cout << "Found character " << c << " in our string " << std::endl;
+    }
+    else
+    {
+        std::cout << "Didn't find our character" << std::endl;
+    }
+
     return 0;
 }
