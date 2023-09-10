b687d2f5c9163308cadd39f088f32ac639424356
Arturs <xyzdelete@protonmail.com>
2023-09-10 11:24:19 +0300

    212. Function return type deduction

--- 19.GettingThingsOutOfFuntions/19.8FunctionReturnTypeDeduction/main.cpp
+++ 19.GettingThingsOutOfFuntions/19.8FunctionReturnTypeDeduction/main.cpp
@@ -13,21 +13,26 @@ auto process_number(int value){
 }
 */
 
-auto process_number(int value){
-    if(value < 10){
+auto process_number(int value)
+{
+    if (value < 10)
+    {
         return static_cast<double>(22); // returning int literal
-    }else{
+    }
+    else
+    {
         return 33.1; // returning double literal
     }
 }
 
 
 
-int main(){
+int main()
+{
 
     auto result = process_number(17);
     std::cout << "result : " << result << std::endl;
-    std::cout << "sizeof(result)  : " << sizeof(result) <<  std::endl;
+    std::cout << "sizeof(result)  : " << sizeof(result) << std::endl;
     std::cout << "sizeof(int) : " << sizeof(int) << std::endl;
     std::cout << "sizeof(double) : " << sizeof(double) << std::endl;
 
