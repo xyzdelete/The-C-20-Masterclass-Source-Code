4f4bd8a90d3b19b8fcaf768418130ee528b836ae
Arturs <xyzdelete@protonmail.com>
2023-09-11 09:57:50 +0300

    222. Overloading with reference parameters

--- 20.FunctionOverloading/20.4OverloadingWithReferenceParameters/main.cpp
+++ 20.FunctionOverloading/20.4OverloadingWithReferenceParameters/main.cpp
@@ -2,36 +2,41 @@
 #include <string>
 
 //Ambiguous calls
-void say_my_name( const std::string& name){
+void say_my_name(const std::string& name)
+{
     std::cout << "Your name is (ref) : " << name << std::endl;
 }
 
-void say_my_name( std::string name){
+void say_my_name(std::string name)
+{
     std::cout << "Your name is (non ref) : " << name << std::endl;
 }
 
 //Implicit conversions with references
 
-double max(double a, double b){
-    std::cout<< "double max called" << std::endl;
-    return (a>b)?a:b;
+double max(double a, double b)
+{
+    std::cout << "double max called" << std::endl;
+    return (a > b) ? a : b;
 }
 
 
 //int& max(int& a, int& b){
-const int& max(const int& a,const int& b){
+const int& max(const int& a, const int& b)
+{
     std::cout << "int max called" << std::endl;
-    return (a>b)?a:b;
+    return (a > b) ? a : b;
 }
 
-int main(){
+int main()
+{
 
-    char a{6};
-    char b{9};
+    char a { 6 };
+    char b { 9 };
 
 
-    auto result = max(a,b);
+    auto result = max(a, b);
 
-   
+
     return 0;
 }
