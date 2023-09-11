157bf36a798e4e379dd4b0c896713f08d33b90f4
Arturs <xyzdelete@protonmail.com>
2023-09-11 09:40:20 +0300

    221. Overloading with pointer parameters

--- 20.FunctionOverloading/20.3OverloadingWithPointerParameters/main.cpp
+++ 20.FunctionOverloading/20.3OverloadingWithPointerParameters/main.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
 
-double max(double * numbers, size_t count){
+double max(double* numbers, size_t count)
+{
     std::cout << "doubles overload called" << std::endl;
-    double maximum{0};
-    
-    for(size_t i{0}; i < count ;++i){
-        if(numbers[i]> maximum)
+    double maximum { 0 };
+
+    for (size_t i { 0 }; i < count ; ++i)
+    {
+        if (numbers[i] > maximum)
             maximum = numbers[i];
     }
-    return maximum;   
+    return maximum;
 }
 
-int max(int * numbers, size_t count){
+int max(int* numbers, size_t count)
+{
     std::cout << "ints overload called" << std::endl;
 
-    int maximum{0};
-    
-    for(size_t i{0}; i < count ;++i){
-        if(numbers[i]> maximum)
+    int maximum { 0 };
+
+    for (size_t i { 0 }; i < count ; ++i)
+    {
+        if (numbers[i] > maximum)
             maximum = numbers[i];
     }
-    return maximum;   
+    return maximum;
 }
 
 
 
-int main(){
+int main()
+{
 
-	double  doubles[] {10.0,30.0,12.3};
-	int  ints[] {1,2,5,2,8,4};
+    double  doubles[] { 10.0,30.0,12.3 };
+    int  ints[] { 1,2,5,2,8,4 };
 
-    auto result = max(ints,std::size(ints));
+    auto result = max(ints, std::size(ints));
     std::cout << "result : " << result << std::endl;
-   
+
     return 0;
 }
