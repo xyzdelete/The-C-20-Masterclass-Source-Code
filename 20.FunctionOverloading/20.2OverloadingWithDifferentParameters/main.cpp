bde687c1ae58041737fe6bc6c4b146adf9e83cd7
Arturs <xyzdelete@protonmail.com>
2023-09-11 09:26:21 +0300

    220. Overloading with different parameters

--- 20.FunctionOverloading/20.2OverloadingWithDifferentParameters/main.cpp
+++ 20.FunctionOverloading/20.2OverloadingWithDifferentParameters/main.cpp
@@ -1,46 +1,53 @@
 #include <iostream>
 
 
-int max(int a, int b){
+int max(int a, int b)
+{
     std::cout << "int overload called" << std::endl;
-    return (a>b)? a : b; 
+    return (a > b) ? a : b;
 }
 
-double max(double a, double b){
+double max(double a, double b)
+{
     std::cout << "double overload called" << std::endl;
-    return (a>b)? a : b; 
+    return (a > b) ? a : b;
 }
 
-double max(int a, double b){
+double max(int a, double b)
+{
     std::cout << "(int,double) overload called" << std::endl;
-    return (a>b)? a : b; 
+    return (a > b) ? a : b;
 }
 
-double max(double a, int b){
+double max(double a, int b)
+{
     std::cout << "(double,int) overload called" << std::endl;
-    return (a>b)? a : b; 
+    return (a > b) ? a : b;
 }
 
-double max(double a, int b,int c){
+double max(double a, int b, int c)
+{
     std::cout << "(double,int,int) overload called" << std::endl;
     return a;
 }
 
-std::string_view  max(std::string_view a, std::string_view b){
+std::string_view  max(std::string_view a, std::string_view b)
+{
     std::cout << "(string_view,string_view) overload called" << std::endl;
-     return (a>b)? a : b;
+    return (a > b) ? a : b;
 }
 
-int main(){
+int main()
+{
 
-    int x{4};
-    int y{9};
-    double a{5.4};
-    double b{7.4};
+    int x { 4 };
+    int y { 9 };
+    double a { 5.4 };
+    double b { 7.4 };
 
-    auto result = max(b,x,y);
+    auto result = max(b, x, y);
 
-    max("Hello","World");
-   
+    max("Hello", "World");
+
     return 0;
 }
