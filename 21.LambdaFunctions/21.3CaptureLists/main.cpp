92b97b733163d61d6a5ef06d9fd1b0e84f0fb9b5
Arturs <xyzdelete@protonmail.com>
2023-09-12 09:53:05 +0300

    230. Capture lists

--- 21.LambdaFunctions/21.3CaptureLists/main.cpp
+++ 21.LambdaFunctions/21.3CaptureLists/main.cpp
@@ -1,46 +1,48 @@
 #include <iostream>
 
-int main(){
-
+int main()
+{
     //Capture lists
     /*
      double a{10};
      double b{20};
-     
+
      auto func = [a,b](){
          std::cout  << "a + b : " << a + b << std::endl;
      };
      func();
      */
 
-    //Capturing by value
-    /*
-     int c{42};
-     
-     auto func = [c](){
-         std::cout << "Inner value : " << c << " &inner : " <<&c <<  std::endl;
-     };
-     
-     for(size_t i{} ; i < 5 ;++i){
-         std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
-         func();
-         ++c;
-     }
+     //Capturing by value
+     /*
+      int c{42};
 
-     */
+      auto func = [c](){
+          std::cout << "Inner value : " << c << " &inner : " <<&c <<  std::endl;
+      };
 
-    //Capture by reference
-     int c{42};
-     
-     auto func = [&c](){
-         std::cout << "Inner value : " << c << " &inner : " <<&c <<  std::endl;
-     };
-     
-     for(size_t i{} ; i < 5 ;++i){
-         std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
-         func();
-         ++c;
-     }
-    
+      for(size_t i{} ; i < 5 ;++i){
+          std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
+          func();
+          ++c;
+      }
+
+      */
+
+      //Capture by reference
+    int c { 42 };
+
+    auto func = [&c]()
+        {
+            std::cout << "Inner value : " << c << " &inner : " << &c << std::endl;
+        };
+
+    for (size_t i {} ; i < 5 ; ++i)
+    {
+        std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
+        func();
+        ++c;
+    }
+
     return 0;
 }
