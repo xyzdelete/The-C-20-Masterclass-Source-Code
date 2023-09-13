1ffdb7ad83d7b41a63347e7e766240e9678ee93e
Arturs <xyzdelete@protonmail.com>
2023-09-13 09:45:01 +0300

    235. Inline functions

--- 22.FunctionsTheMisfits/22.3InlineFunctions/main.cpp
+++ 22.FunctionsTheMisfits/22.3InlineFunctions/main.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 
-inline int max(int a, int b){
-    if(a> b){
+inline int max(int a, int b)
+{
+    if (a > b)
+    {
         return a;
-    }else{
+    }
+    else
+    {
         return b;
     }
 }
 
-int main(){
+int main()
+{
 
-    int a{15};
-    int b{8};
+    int a { 15 };
+    int b { 8 };
 
     //std::cout << "max : " << max(a,b) << std::endl;
 
     //What the compiler might do to inline your function call
     std::cout << "max : " ;
-    if(a> b){
-       std::cout << a << std::endl;
-    }else{
+    if (a > b)
+    {
+        std::cout << a << std::endl;
+    }
+    else
+    {
         std::cout << b << std::endl;
     }
 
-   
+
     return 0;
 }
