d928fccf5c6e7a0c808a6a3255fcf406d9b86e65
Arturs <xyzdelete@protonmail.com>
2023-09-13 09:27:41 +0300

    234. Static variables

--- 22.FunctionsTheMisfits/22.2StaticVariables/main.cpp
+++ 22.FunctionsTheMisfits/22.2StaticVariables/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 
-size_t dog_count{0};// Global scope
+size_t dog_count { 0 };// Global scope
 
 
-unsigned int add_student(){
-     static unsigned int student_count{0}; // The scope of this var is in add_student,
-							  // but it's life time goes beyond the function execution.
+unsigned int add_student()
+{
+    static unsigned int student_count { 0 }; // The scope of this var is in add_student,
+    // but it's life time goes beyond the function execution.
     student_count++;
     ++dog_count;
     return student_count;
 }
 
-void do_something(){
+void do_something()
+{
     //--student_count; // Compiler error
     ++dog_count;
 }
 
-int main(){
+int main()
+{
 
     /*
     std::cout << "student count : " << add_student()  << std::endl;//1
@@ -24,9 +27,10 @@ int main(){
     std::cout << "student count : " << add_student()  << std::endl;//3
     */
 
-   for(size_t i {} ; i < 20 ;++i){
-       std::cout << "student count : " << add_student()  << std::endl;
-   }
-   
+    for (size_t i {} ; i < 20 ; ++i)
+    {
+        std::cout << "student count : " << add_student() << std::endl;
+    }
+
     return 0;
 }
