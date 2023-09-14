9badf386194dfe357244e03a4049e6e0253ffad2
Arturs <xyzdelete@protonmail.com>
2023-09-14 10:48:00 +0300

    241. Debugging in VS Code

--- 23.FunctionCallStackD_ebugging/23.4D_ebuggingInV_S_C0de/main.cpp
+++ 23.FunctionCallStackD_ebugging/23.4D_ebuggingInV_S_C0de/main.cpp
@@ -1,29 +1,32 @@
 
 #include <iostream>
 
-int& adjust( int& input);
-int sum(int x , int y);
+int& adjust(int& input);
+int sum(int x, int y);
 
 
-int main(int argc, char **argv){
+int main(int argc, char** argv)
+{
 
-	int a{10};
-	int b{12};
+	int a { 10 };
+	int b { 12 };
 
-	int summation  =  sum( a , b);
-	std::cout << "sum : " 
+	int summation = sum(a, b);
+	std::cout << "sum : "
 		<< summation << std::endl;
 
 	return 0;
 }
 
-int& adjust( int& input){
-	int adjustment{2};
+int& adjust(int& input)
+{
+	int adjustment { 2 };
 	input += adjustment;
 	return input;
 }
 
-int sum(int x , int y){
+int sum(int x, int y)
+{
 
 	int result = x + y;
 	adjust(result);
