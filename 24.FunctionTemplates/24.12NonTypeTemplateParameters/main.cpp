4bd3b30580d6c57082c4908ad9891591261afd31
Arturs <xyzdelete@protonmail.com>
2023-09-16 12:54:58 +0300

    258. Non type template parameters

--- 24.FunctionTemplates/24.12NonTypeTemplateParameters/main.cpp
+++ 24.FunctionTemplates/24.12NonTypeTemplateParameters/main.cpp
@@ -15,13 +15,14 @@ bool is_valid(T collection[] ,size_t size)
 
 //Version making a little more sense for the task
 template <typename T>
-bool is_valid(T collection[], int threshold,size_t size)
+bool is_valid(T collection[], int threshold, size_t size)
 {
-	T sum{};
-	for(size_t i{ 0 }; i < size; ++i) {
-		sum += collection[i];
-	}
-	return (sum > threshold) ? true : false;
+    T sum {};
+    for (size_t i { 0 }; i < size; ++i)
+    {
+        sum += collection[i];
+    }
+    return (sum > threshold) ? true : false;
 }
 
 
@@ -35,13 +36,14 @@ T process (T a, T b){
 }
 */
 
-int main(){
+int main()
+{
 
-	double temperatures[] {10.0,20.0,30.0,40.0,50.0,100.0};
+    double temperatures[] { 10.0,20.0,30.0,40.0,50.0,100.0 };
 
-    auto result = is_valid<double>(temperatures,200,std::size(temperatures));
+    auto result = is_valid<double>(temperatures, 200, std::size(temperatures));
     std::cout << std::boolalpha;
     std::cout << "result : " << result << std::endl;
-   
+
     return 0;
 }
