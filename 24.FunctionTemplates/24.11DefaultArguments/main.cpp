1cf9bdeea3ff8e12a78a9295880cbf4f63102fd3
Arturs <xyzdelete@protonmail.com>
2023-09-16 12:08:43 +0300

    257. Default arguments

--- 24.FunctionTemplates/24.11DefaultArguments/main.cpp
+++ 24.FunctionTemplates/24.11DefaultArguments/main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 
-
-template <typename ReturnType = double , typename T, typename P>
-ReturnType maximum ( T a, P b){
+template <typename ReturnType = double, typename T, typename P>
+ReturnType maximum(T a, P b)
+{
     return (a > b) ? a : b;
 }
 
-template < typename T, typename P,typename ReturnType = double>
-ReturnType minimum ( T a, P b){
+template < typename T, typename P, typename ReturnType = double>
+ReturnType minimum(T a, P b)
+{
     return (a < b) ? a : b;
 }
 
-int main(){
+int main()
+{
 
-    double a{6};
-    double b{9.5};
+    double a { 6 };
+    double b { 9.5 };
 
-    auto result = minimum <int,double,int> (a,b);
+    auto result = minimum <int, double, int>(a, b);
     std::cout << "result : " << result << std::endl;
     std::cout << "sizeof(result) : " << sizeof(result) << std::endl;//4
-   
+
     return 0;
 }
