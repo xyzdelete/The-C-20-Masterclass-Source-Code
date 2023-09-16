b23916567e4b218f94351fe7eaea70de974b1e6c
Arturs <xyzdelete@protonmail.com>
2023-09-16 10:44:57 +0300

    254. Template return type deduction with auto

--- 24.FunctionTemplates/24.8TemplateReturnTypeDeductionWithAuto/main.cpp
+++ 24.FunctionTemplates/24.8TemplateReturnTypeDeductionWithAuto/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 
 template <typename T, typename P>
-auto maximum ( T a, P b){
-    return (a > b)? a : b;
+auto maximum(T a, P b)
+{
+    return (a > b) ? a : b;
 }
 
-int main(){
-
+int main()
+{
     //Largest type is going to be deduced as return type
 
-	auto max1 = maximum ('e', 33); // double return type deduced
+    auto max1 = maximum('e', 33); // double return type deduced
     std::cout << "max1 : " << max1 << std::endl;
     std::cout << "size of max1 : " << sizeof(max1) << std::endl;
-   
+
     return 0;
 }
 
