279ec3e14b4134afd6984debeaa6a9d2fe5b1583
Arturs <xyzdelete@protonmail.com>
2023-09-17 11:24:37 +0300

    267. Zooming in on the requires clause

--- 25.Concepts/25.04ZoomingInOnRequiresClause/main.cpp
+++ 25.Concepts/25.04ZoomingInOnRequiresClause/main.cpp
@@ -2,7 +2,8 @@
 #include <concepts>
 
 template <typename T>
-concept TinyType = requires (T t){
+concept TinyType = requires (T t)
+{
     sizeof(T) <= 4; // Simple requirement : Only enforces syntax
     requires sizeof(T) <= 4; // Nested requirements
 };
@@ -10,35 +11,38 @@ concept TinyType = requires (T t){
 
 //Compound requirement
 template <typename T>
-concept Addable = requires (T a, T b) {
-	//noexcept is optional
-	{a + b} -> std::convertible_to<int>; //Compound requirement
-	//Checks if a + b is valid syntax, doesn't throw expetions(optional) , and the result
-	//is convertible to int(optional)
+concept Addable = requires (T a, T b)
+{
+    //noexcept is optional
+    { a + b } -> std::convertible_to<int>; //Compound requirement
+    //Checks if a + b is valid syntax, doesn't throw expetions(optional) , and the result
+    //is convertible to int(optional)
 };
 
 
-Addable auto add( Addable auto a, Addable auto b){
+Addable auto add(Addable auto a, Addable auto b)
+{
     return a + b;
 }
 
 
 
 
-int main(){
+int main()
+{
 
-    double x{67};
-    double y{56};
+    double x { 67 };
+    double y { 56 };
 
     //std::string x{"Hello"};
     //std::string y{"World"};
 
     //auto s = x + y;
 
-    auto result  = add(x,y);
+    auto result = add(x, y);
     std::cout << "result : " << result << std::endl;
     std::cout << "sizeof(result) : " << sizeof(result) << std::endl;
 
-   
+
     return 0;
 }
