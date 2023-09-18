70fc6fa0c715cadfef8676d932a79cbe4075528d
Arturs <xyzdelete@protonmail.com>
2023-09-18 10:44:30 +0300

    275. Setters and Getters

--- 26.Classes/26.5SettersAndGetters/main.cpp
+++ 26.Classes/26.5SettersAndGetters/main.cpp
@@ -1,45 +1,52 @@
 #include <iostream>
 
-const double PI {3.1415926535897932384626433832795};
+const double PI { 3.1415926535897932384626433832795 };
 
 class Cylinder {
-    public : 
-        //Constctors
-        Cylinder() = default;
-        Cylinder(double rad_param,double height_param){
-            base_radius = rad_param;
-            height = height_param;
-        }
-        //Functions (methods)
-        double volume(){
-            return PI * base_radius * base_radius * height;
-        }
-        
-        //Setter and getter methods
-        double get_base_radius(){
-            return base_radius;
-        }
-        double get_height(){
-            return height;
-        }
+public:
+    //Constctors
+    Cylinder() = default;
+    Cylinder(double rad_param, double height_param)
+    {
+        base_radius = rad_param;
+        height = height_param;
+    }
+    //Functions (methods)
+    double volume()
+    {
+        return PI * base_radius * base_radius * height;
+    }
 
-        void set_base_radius(double rad_param){
-            base_radius = rad_param;
-        }
+    //Setter and getter methods
+    double get_base_radius()
+    {
+        return base_radius;
+    }
+    double get_height()
+    {
+        return height;
+    }
 
-        void set_height(double height_param){
-            height = height_param;
-        }
-    private : 
-        //Member variables
-        double base_radius{1};
-        double height{1};
+    void set_base_radius(double rad_param)
+    {
+        base_radius = rad_param;
+    }
+
+    void set_height(double height_param)
+    {
+        height = height_param;
+    }
+private:
+    //Member variables
+    double base_radius { 1 };
+    double height { 1 };
 };
 
 
 
-int main(){
-    Cylinder cylinder1(10,10);
+int main()
+{
+    Cylinder cylinder1(10, 10);
     std::cout << "volume : " << cylinder1.volume() << std::endl;
 
     //Modify our object
