4aede298b9a3de67f19922ef4c73bf897f0f05f3
Arturs <xyzdelete@protonmail.com>
2023-09-18 12:19:36 +0300

    279. Destructors

--- 26.Classes/26.9Destructors/main.cpp
+++ 26.Classes/26.9Destructors/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <string_view>
 
-class Dog{
-    public : 
-        Dog() = default;
-        Dog(std::string_view name_param, std::string_view breed_param, int  age_param);
-        ~Dog();
+class Dog {
+public:
+    Dog() = default;
+    Dog(std::string_view name_param, std::string_view breed_param, int  age_param);
+    ~Dog();
 
-    private : 
-        std::string name;
-        std::string breed;
-        int * p_age{nullptr};
+private:
+    std::string name;
+    std::string breed;
+    int* p_age { nullptr };
 };
-Dog::Dog(std::string_view name_param, std::string_view breed_param, int  age_param){
+Dog::Dog(std::string_view name_param, std::string_view breed_param, int  age_param)
+{
     name = name_param;
     breed = breed_param;
     p_age = new int;
@@ -20,23 +21,26 @@ Dog::Dog(std::string_view name_param, std::string_view breed_param, int  age_par
     std::cout << "Dog constructor called for " << name << std::endl;
 }
 
-Dog::~Dog(){
+Dog::~Dog()
+{
     delete p_age;
     std::cout << "Dog destructor called for : " << name << std::endl;
 }
 
-void some_func(){
-    Dog* p_dog = new Dog("Fluffy","Shepherd",2);
+void some_func()
+{
+    Dog* p_dog = new Dog("Fluffy", "Shepherd", 2);
 
     delete p_dog;// Causes for the destructor of Dog to be called
 }
 
 
 
-int main(){ 
+int main()
+{
 
     some_func();
-    
+
 
     std::cout << "Done!" << std::endl;
     return 0;
