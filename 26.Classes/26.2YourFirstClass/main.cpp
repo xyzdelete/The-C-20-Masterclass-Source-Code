8741cff1deb3bdc8bca2b98e200c4a3eebd3dbea
Arturs <xyzdelete@protonmail.com>
2023-09-18 09:32:23 +0300

    272. Your First Class

--- 26.Classes/26.2YourFirstClass/main.cpp
+++ 26.Classes/26.2YourFirstClass/main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 
-const double PI {3.1415926535897932384626433832795};
+const double PI { 3.1415926535897932384626433832795 };
 
 class Cylinder {
-    public : 
-        //Functions (methods)
-        double volume(){
-            return PI * base_radius * base_radius * height;
-        }
+    //Functions (methods)
+    double volume()
+    {
+        return PI * base_radius * base_radius * height;
+    }
 
-    public : 
-        //Member variables
-        double base_radius{1};
-        double height{1};
+public:
+    //Member variables
+    double base_radius { 1 };
+    double height { 1 };
 };
 
 
-int main(){
+int main()
+{
 
     Cylinder cylinder1; // Objects
     std::cout << "volume : " << cylinder1.volume() << std::endl;
@@ -27,9 +28,9 @@ int main(){
 
     std::cout << "volume : " << cylinder1.volume() << std::endl;
 
-    cylinder1.height =8;
+    cylinder1.height = 8;
 
     std::cout << "volume : " << cylinder1.volume() << std::endl;
-   
+
     return 0;
 }
