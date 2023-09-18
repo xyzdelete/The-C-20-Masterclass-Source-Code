36815a1b2ebe4515a9d7c9f531141dc430b6b7b7
Arturs <xyzdelete@protonmail.com>
2023-09-18 11:17:18 +0300

    278. Arrow pointer call notation

--- 26.Classes/26.8ManagingClassObjectsThroughPointers/main.cpp
+++ 26.Classes/26.8ManagingClassObjectsThroughPointers/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "cylinder.h"
 
-int main(){
+int main()
+{
 
-    Cylinder cylinder1(10,10);
+    Cylinder cylinder1(10, 10);
 
     cylinder1.volume();
 
@@ -14,7 +15,7 @@ int main(){
     std::cout << "volume : " << p_cylinder1->volume() << std::endl;
 
     //Create a cylinder heap object through the new operator
-    Cylinder* p_cylinder2 = new Cylinder(100,2); // Heap
+    Cylinder* p_cylinder2 = new Cylinder(100, 2); // Heap
 
     std::cout << "volume(clylinder2) : " << p_cylinder2->volume() << std::endl;
     std::cout << "base_rad(cylinder2) : " << p_cylinder2->get_base_radius() << std::endl;
@@ -23,7 +24,7 @@ int main(){
 
 
     delete p_cylinder2;
-   
-    
+
+
     return 0;
 }
