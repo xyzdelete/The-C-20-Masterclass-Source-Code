18f5395640c941766f6f182b97e77dc83af9b80a
Arturs <xyzdelete@protonmail.com>
2023-09-19 10:45:55 +0300

    289. Getters that work as setters

--- 27.ZoomingInOnClassObjects/27.5GettersThatDoubleAsSetters/main.cpp
+++ 27.ZoomingInOnClassObjects/27.5GettersThatDoubleAsSetters/main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include "dog.h"
 
-int main(){
+int main()
+{
+    const Dog dog1("Fluffy", "Shepherd", 2);
+    dog1.print_info();
+    std::cout << "dog name : " << dog1.name() << std::endl; // Getter
 
-   const Dog dog1("Fluffy","Shepherd",2);
-   dog1.print_info();
-   std::cout << "dog name : " << dog1.name() << std::endl; // Getter
-
-   //Modify the object
-   //dog1.name() = "Milou"; // Setter
-   dog1.print_info();
-
-
-
+    //Modify the object
+    //dog1.name() = "Milou"; // Setter
+    dog1.print_info();
 
     return 0;
 }
