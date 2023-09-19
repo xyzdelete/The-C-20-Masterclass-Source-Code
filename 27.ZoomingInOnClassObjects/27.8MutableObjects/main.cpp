0af30f49ce2584912f148bd284c23d94dc532020
Arturs <xyzdelete@protonmail.com>
2023-09-19 15:14:11 +0300

    292. Mutable Member variables

--- 27.ZoomingInOnClassObjects/27.8MutableObjects/main.cpp
+++ 27.ZoomingInOnClassObjects/27.8MutableObjects/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include "dog.h"
 
-int main(){
-
-    Dog dog1("Fluffy","Shepherd",2);
+int main()
+{
+    Dog dog1("Fluffy", "Shepherd", 2);
     dog1.print_info(); //1
     dog1.print_info();//2
 
-    for(size_t i{0}; i <10;++i){
+    for (size_t i { 0 }; i < 10; ++i)
+    {
         dog1.print_info();
     }
 
