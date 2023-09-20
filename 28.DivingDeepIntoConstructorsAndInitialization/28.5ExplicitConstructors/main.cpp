14fcd7fff8fb24d4ddc2d6a765c58396771dde64
Arturs <xyzdelete@protonmail.com>
2023-09-20 10:25:55 +0300

    299. Explicit constructors

--- 28.DivingDeepIntoConstructorsAndInitialization/28.5ExplicitConstructors/main.cpp
+++ 28.DivingDeepIntoConstructorsAndInitialization/28.5ExplicitConstructors/main.cpp
@@ -2,22 +2,24 @@
 #include "square.h"
 
 //Is square1 > to square2 ? true or false
-bool compare( const Square& square1 ,const Square& square2){
+bool compare(const Square& square1, const Square& square2)
+{
     return (square1.surface() > square2.surface()) ? true : false;
 }
 
-int main(){
+int main()
+{
 
     Square s1(100.0);
     Square s2(20.0);
 
     std::cout << std::boolalpha;
-    std::cout << "s1 > s2 : " << compare(s1,s2) << std::endl; // false
+    std::cout << "s1 > s2 : " << compare(s1, s2) << std::endl; // false
 
     //Implicit conversions
-    std::cout << "s1 > 45.9 : " << compare(s1,45.9) << std::endl; // false
+    std::cout << "s1 > 45.9 : " << compare(s1, 45.9) << std::endl; // false
 
 
-   
+
     return 0;
 }
