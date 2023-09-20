d5726ec37e32feed936b389df8ec73f3b11c807c
Arturs <xyzdelete@protonmail.com>
2023-09-20 09:32:34 +0300

    296. Default parameters for constructors

--- 28.DivingDeepIntoConstructorsAndInitialization/28.2DefaultParametersForConstructors/main.cpp
+++ 28.DivingDeepIntoConstructorsAndInitialization/28.2DefaultParametersForConstructors/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "cylinder.h"
 
-
-int main(){
-
+int main()
+{
     Cylinder c1(4);
     std::cout << "volume : " << c1.volume() << std::endl;
-   
+
     return 0;
 }
