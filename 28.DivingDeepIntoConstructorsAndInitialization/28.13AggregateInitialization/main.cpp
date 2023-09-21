e5c2ff6fa203a259fbd738638babee2e99a16e5b
Arturs <xyzdelete@protonmail.com>
2023-09-21 09:56:10 +0300

    307. Aggregate Initialization

--- 28.DivingDeepIntoConstructorsAndInitialization/28.13AggregateInitialization/main.cpp
+++ 28.DivingDeepIntoConstructorsAndInitialization/28.13AggregateInitialization/main.cpp
@@ -6,17 +6,18 @@ struct Point
     double y;
 };
 
-void print_point(const Point& p){
+void print_point(const Point& p)
+{
     std::cout << "Point [ x : " << p.x << " , y : " << p.y << "]" << std::endl;
 }
 
+int main()
+{
 
-int main(){
-
-    Point p1{10,20};
+    Point p1 { 10,20 };
     print_point(p1);
 
-    int scores[] {44,62,67,82,98,43,2,5,67};
-   
+    int scores[] { 44,62,67,82,98,43,2,5,67 };
+
     return 0;
 }
