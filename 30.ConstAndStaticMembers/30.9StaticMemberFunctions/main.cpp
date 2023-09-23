6fc64c485f78f9c9d401168ce8ac95b3483cdf85
Arturs <xyzdelete@protonmail.com>
2023-09-23 12:33:42 +0300

    323. Static member functions

--- 30.ConstAndStaticMembers/30.9StaticMemberFunctions/main.cpp
+++ 30.ConstAndStaticMembers/30.9StaticMemberFunctions/main.cpp
@@ -2,15 +2,14 @@
 #include "point.h"
 
 
-int main(){
+int main()
+{
+    // std::cout << "Point count : " << Point::get_point_count() << std::endl;
 
-   // std::cout << "Point count : " << Point::get_point_count() << std::endl;
+    Point p1 { 6,7 };
 
-    Point p1{6,7};
-
-   // std::cout << "Point count : " << p1.get_point_count() << std::endl;
+    // std::cout << "Point count : " << p1.get_point_count() << std::endl;
     p1.print_info(p1) ;
 
- 
     return 0;
 }
