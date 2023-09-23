c6e693ddf5ca7594f98538e6295e454e3a208626
Arturs <xyzdelete@protonmail.com>
2023-09-23 10:28:48 +0300

    319. Static constants

--- 30.ConstAndStaticMembers/30.5StaticConstants/main.cpp
+++ 30.ConstAndStaticMembers/30.5StaticConstants/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include "cylinder.h"
 
-
-
-int main(){
-    Cylinder cylinder1(3.3,10.0);
+int main()
+{
+    Cylinder cylinder1(3.3, 10.0);
     std::cout << "Volume of cylinder : " << cylinder1.volume() << std::endl;
     std::cout << "Cylinder default color : " << Cylinder::default_color << std::endl;
     std::cout << "Cylinder default color : " << cylinder1.default_color << std::endl;
-	
+
     return 0;
 }
