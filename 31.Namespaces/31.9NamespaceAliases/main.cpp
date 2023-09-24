b06c589f84214d244d6c63d574ef4c4aac1d22e2
Arturs <xyzdelete@protonmail.com>
2023-09-24 11:02:12 +0300

    335. Namespace Aliases

--- 31.Namespaces/31.9NamespaceAliases/main.cpp
+++ 31.Namespaces/31.9NamespaceAliases/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
-namespace Level1{
-    namespace Level2{
-        namespace Level3{
-            const double weight{33.33};
+namespace Level1
+{
+    namespace Level2
+    {
+        namespace Level3
+        {
+            const double weight { 33.33 };
         }
     }
 }
 
 
-int main(){
+int main()
+{
 
     //Namespace aliases
     namespace Data = Level1::Level2::Level3;
@@ -17,6 +21,6 @@ int main(){
     //std::cout << "weight : " << Level1::Level2::Level3::weight << std::endl;
     std::cout << "weight : " << Data::weight << std::endl;
     std::cout << "weight : " << Data::weight << std::endl;
-    
+
     return 0;
 }
