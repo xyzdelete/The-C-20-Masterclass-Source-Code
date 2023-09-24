f7cd19111ef90339b6da5d371eca4ae04c79343f
Arturs <xyzdelete@protonmail.com>
2023-09-24 10:55:19 +0300

    333. Anounymous Namespaces

--- 31.Namespaces/31.7AnonymousNamespaces/main.cpp
+++ 31.Namespaces/31.7AnonymousNamespaces/main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 
-namespace{
+namespace
+{
     double add(double a, double b); // Declaration
-} 
+}
 
 
-int main(){
+int main()
+{
 
-    double result = add(10.2,20.2);
+    double result = add(10.2, 20.2);
     std::cout << "result : " << result << std::endl;
-   
+
     return 0;
 }
 
 
-namespace{
-    double add(double a, double b){
+namespace
+{
+    double add(double a, double b)
+    {
         return a + b;
     }
-} 
+}
