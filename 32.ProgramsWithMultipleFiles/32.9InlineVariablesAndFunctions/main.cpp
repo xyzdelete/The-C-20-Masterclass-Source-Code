8fd021321bca7f70aa2f8d2a0aa73c98e4338fef
Arturs <xyzdelete@protonmail.com>
2023-09-25 12:11:15 +0300

    344. Inline variables and functions

--- 32.ProgramsWithMultipleFiles/32.9InlineVariablesAndFunctions/main.cpp
+++ 32.ProgramsWithMultipleFiles/32.9InlineVariablesAndFunctions/main.cpp
@@ -4,10 +4,10 @@
 void print_age_utility1();
 void print_age_utility2();
 
-int main(){
+int main()
+{
+    print_age_utility1();
+    print_age_utility2();
 
-   print_age_utility1();
-   print_age_utility2();
-   
     return 0;
 }
