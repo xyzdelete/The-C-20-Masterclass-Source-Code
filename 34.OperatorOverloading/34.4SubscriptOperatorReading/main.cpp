a500d744ca644b2c070e87b8da51a7fcf3122e5b
Arturs <xyzdelete@protonmail.com>
2023-09-28 11:47:13 +0300

    362. Subscript Operator for Reading

--- 34.OperatorOverloading/34.4SubscriptOperatorReading/main.cpp
+++ 34.OperatorOverloading/34.4SubscriptOperatorReading/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "point.h"
 
-
-int main(){
-
-    Point p1(10,20);
+int main()
+{
+    Point p1(10, 20);
 
     //std::cout << "p1.x : " << p1[0] << std::endl; // x coordinate : 10
     std::cout << "p1.x : " << p1.operator[](0) << std::endl; // x coordinate : 10
