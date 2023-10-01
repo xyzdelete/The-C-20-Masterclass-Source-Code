c5e7f087e927e39fc4912d0796949d940337e57b
Arturs <xyzdelete@protonmail.com>
2023-10-01 13:02:14 +0300

    378. Functors

--- 34.OperatorOverloading/34.20Functors/main.cpp
+++ 34.OperatorOverloading/34.20Functors/main.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <string>
 
-class Print{
-    public : 
-    void operator()(const std::string& name) const{
+class Print {
+public:
+    void operator()(const std::string& name) const
+    {
         std::cout << "The name is : " << name << std::endl;
     }
 
     std::string operator()(const std::string& last_name,
-                            const std::string& first_name) const {
+                            const std::string& first_name) const
+    {
         return (last_name + " " + first_name);
     }
 };
 
-void do_something(const Print& printer){
+void do_something(const Print& printer)
+{
     printer("Snow");
 }
 
 
-int main(){
+int main()
+{
 
     Print print;
     print("John");
     do_something(print);
-    std::cout << print("Daniel","Gray") << std::endl;
-   
+    std::cout << print("Daniel", "Gray") << std::endl;
+
     return 0;
 }
