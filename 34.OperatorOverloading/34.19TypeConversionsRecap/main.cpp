f1a6a80cd453213ea84cc25b257a77ae7b906e61
Arturs <xyzdelete@protonmail.com>
2023-10-01 12:50:40 +0300

    377. Type Conversions Recap

--- 34.OperatorOverloading/34.19TypeConversionsRecap/main.cpp
+++ 34.OperatorOverloading/34.19TypeConversionsRecap/main.cpp
@@ -1,15 +1,16 @@
- #include <iostream>
+#include <iostream>
 #include "point.h"
 #include "number.h"
 
-void do_something_with_point(const Point& p){
+void do_something_with_point(const Point& p)
+{
     std::cout << "point : " << p << std::endl;
 }
 
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-	Point p1(10,10);
+    Point p1(10, 10);
     Number n1(22);
 
     //p1 = n1;
@@ -22,6 +23,6 @@ int main(int argc, char **argv)
     do_something_with_point(n1);
     // .(1) Constructor
     // .(2) Type conversion operator
-	
-	return 0;
+
+    return 0;
 }
