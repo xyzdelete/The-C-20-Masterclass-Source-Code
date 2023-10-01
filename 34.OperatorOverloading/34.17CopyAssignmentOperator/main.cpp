0ef8c33dd0935ae9094939c402ba7324fe8fd80d
Arturs <xyzdelete@protonmail.com>
2023-10-01 12:02:36 +0300

    375. Copy Assignment Operator

--- 34.OperatorOverloading/34.17CopyAssignmentOperator/main.cpp
+++ 34.OperatorOverloading/34.17CopyAssignmentOperator/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include "point.h"
 
-int main(){
-
-    
-    Point p1(10,10,10);
-    Point p2(20,20,20);
-    Point p77(3,3,3);
+int main()
+{
+    Point p1(10, 10, 10);
+    Point p2(20, 20, 20);
+    Point p77(3, 3, 3);
 
     std::cout << "p1 : " << p1 << std::endl;
     std::cout << "p2 : " << p2 << std::endl;
@@ -27,26 +26,26 @@ int main(){
     std::cout << "-----------" << std::endl;
     std::cout << "p1 : " << p1 << std::endl;
     std::cout << "p2 : " << p2 << std::endl;
-   
-
-   //
-   /*
-   Point p3(30,30,30);
-   Point p4 = p3;
-
-   std::cout << "p3 : " << p3 << std::endl;
-   std::cout << "p4 : " << p4 << std::endl;
-
-   p3.set_data(77);
-
-   std::cout << "-----" << std::endl;
-   std::cout << "p3 : " << p3 << std::endl;
-   std::cout << "p4 : " << p4 << std::endl;
 
 
-   std::cout << "Done!" << std::endl;
-   */
+    //
+    /*
+    Point p3(30,30,30);
+    Point p4 = p3;
 
-    
+    std::cout << "p3 : " << p3 << std::endl;
+    std::cout << "p4 : " << p4 << std::endl;
+
+    p3.set_data(77);
+
+    std::cout << "-----" << std::endl;
+    std::cout << "p3 : " << p3 << std::endl;
+    std::cout << "p4 : " << p4 << std::endl;
+
+
+    std::cout << "Done!" << std::endl;
+    */
+
+
     return 0;
 }
