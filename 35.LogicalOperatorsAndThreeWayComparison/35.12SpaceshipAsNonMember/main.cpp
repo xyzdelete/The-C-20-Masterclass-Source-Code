4146a901896844864c6cb9acda6e8790d7b79e02
Arturs <xyzdelete@protonmail.com>
2023-10-04 10:35:52 +0300

    391. Spaceship operator as a non member

--- 35.LogicalOperatorsAndThreeWayComparison/35.12SpaceshipAsNonMember/main.cpp
+++ 35.LogicalOperatorsAndThreeWayComparison/35.12SpaceshipAsNonMember/main.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 #include "number.h"
 
-class Point{
-    public : 
-    Point(double x, double y)   
-        : m_x{x},m_y{y}
-    {};
+class Point {
+public:
+    Point(double x, double y)
+        : m_x { x }, m_y { y }
+    {
+    };
 
     //Type conversion operator from Number to Point
-    operator Number()const{
+    operator Number()const
+    {
         std::cout << "Converting : " << m_x << std::endl;
         return Number(static_cast<int>(m_x));
     }
-    private : 
-        double m_x{};
-        double m_y{};
+private:
+    double m_x {};
+    double m_y {};
 };
 
-int main(){
+int main()
+{
 
-    Point p1{10,10};
-    Point p2{20,20};
+    Point p1 { 10,10 };
+    Point p2 { 20,20 };
 
-    bool result  = ( p1 > p2);  
+    bool result = (p1 > p2);
     std::cout << std::boolalpha;
-    std::cout << "p1 > p2 : " << result << std::endl;  
-   
-   
+    std::cout << "p1 > p2 : " << result << std::endl;
+
+
     return 0;
 }
