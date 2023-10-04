2f3ab2186e9d3670156efa161792b7034242d572
Arturs <xyzdelete@protonmail.com>
2023-10-04 11:02:21 +0300

    393. Zooming on weak ordering - Example 2

--- 35.LogicalOperatorsAndThreeWayComparison/35.14ZoomingInOnWeakOrderingExample2/main.cpp
+++ 35.LogicalOperatorsAndThreeWayComparison/35.14ZoomingInOnWeakOrderingExample2/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <string>
 
-
-std::weak_ordering case_insensitive_compare(const char* str1, const char* str2) {
+std::weak_ordering case_insensitive_compare(const char* str1, const char* str2)
+{
 
     //Turn them all to uppercase
     std::string string1(str1);
     std::string string2(str2);
 
-    for (auto& c : string1) {
+    for (auto& c : string1)
+    {
         c = toupper(c);
     }
 
-    for (auto& c : string2) {
+    for (auto& c : string2)
+    {
         c = toupper(c);
     }
 
@@ -32,36 +34,40 @@ public:
         : s(str)
     {
     }
-    std::weak_ordering operator<=>(const CaseInsensitiveString& b) const {
+    std::weak_ordering operator<=>(const CaseInsensitiveString& b) const
+    {
         return case_insensitive_compare(s.c_str(), b.s.c_str());
     }
-    std::weak_ordering operator<=>(const char* b) const {
+    std::weak_ordering operator<=>(const char* b) const
+    {
         return case_insensitive_compare(s.c_str(), b);
     }
-	
-	bool operator==(const CaseInsensitiveString& right_operand)const{
-		return (case_insensitive_compare(s.c_str(),right_operand.s.c_str())
-				== std::weak_ordering::equivalent)? true : false;
-	}
+
+    bool operator==(const CaseInsensitiveString& right_operand)const
+    {
+        return (case_insensitive_compare(s.c_str(), right_operand.s.c_str())
+                == std::weak_ordering::equivalent) ? true : false;
+    }
 
 private:
     std::string s;
 };
 
 
-int main(){
+int main()
+{
 
-	CaseInsensitiveString ci_str1("Hello");
-	CaseInsensitiveString ci_str2("ZELLO");
-	
-	std::cout << std::boolalpha;
-	
-	std::cout << "ci_str1 <= ci_str2 : " << (ci_str1 <= ci_str2) << std::endl;
-	std::cout << "ci_str1 <= Kello : " << (ci_str1 <= "Kello") << std::endl;
+    CaseInsensitiveString ci_str1("Hello");
+    CaseInsensitiveString ci_str2("ZELLO");
+
+    std::cout << std::boolalpha;
+
+    std::cout << "ci_str1 <= ci_str2 : " << (ci_str1 <= ci_str2) << std::endl;
+    std::cout << "ci_str1 <= Kello : " << (ci_str1 <= "Kello") << std::endl;
     std::cout << "Kello <= ci_str2 : " << ("Kello" <= ci_str2) << std::endl;
-    
-	//You need to put in a == operator. Compiler won't generate one for you.
-	std::cout << "ci_str1 == ci_str2 : " << (ci_str1 == ci_str2) << std::endl;
-   
+
+    //You need to put in a == operator. Compiler won't generate one for you.
+    std::cout << "ci_str1 == ci_str2 : " << (ci_str1 == ci_str2) << std::endl;
+
     return 0;
 }
