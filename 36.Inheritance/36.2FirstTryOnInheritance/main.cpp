0ff5b4bea439a7bfee4ec02deac978c5ca57fac3
Arturs <xyzdelete@protonmail.com>
2023-10-05 09:57:40 +0300

    398. First try on Inheritance

--- 36.Inheritance/36.2FirstTryOnInheritance/main.cpp
+++ 36.Inheritance/36.2FirstTryOnInheritance/main.cpp
@@ -2,12 +2,13 @@
 #include "player.h"
 
 
-int main(){
+int main()
+{
 
     Player p1("Basketball");
     p1.set_first_name("John");
     p1.set_last_name("Snow");
     std::cout << "player : " << p1 << std::endl;
-   
+
     return 0;
 }
