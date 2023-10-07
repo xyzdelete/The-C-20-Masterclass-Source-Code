e7ddb2cc5089f2065c7f21f86ddb56fff145d233
Arturs <xyzdelete@protonmail.com>
2023-10-07 10:55:40 +0300

    409. Reused Symbols in Inheritance

--- 36.Inheritance/36.13ReusedSymbolsInInheritance/main.cpp
+++ 36.Inheritance/36.13ReusedSymbolsInInheritance/main.cpp
@@ -2,14 +2,14 @@
 #include "child.h"
 
 
-
-int main(){
-	Child child(33);
+int main()
+{
+    Child child(33);
     child.print_var();// Calls the method in Child
     child.Parent::print_var(); // Calls the method in Parent, 
-                               // value in parent just contains junk or whatever
-                               // in class initialization we did.
-							   
+    // value in parent just contains junk or whatever
+    // in class initialization we did.
+
     std::cout << "--------" << std::endl;
     child.show_values();
     return 0;
