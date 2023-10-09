73c6c8a9506c5e5c08390ba4ee4dc839ceb6b002
Arturs <xyzdelete@protonmail.com>
2023-10-09 19:06:57 +0300

    419. Inheritance and polymorphism with static members

--- 37.Polymorphism/37.9InheritanceAndPolymorphismWithStaticMembers/main.cpp
+++ 37.Polymorphism/37.9InheritanceAndPolymorphismWithStaticMembers/main.cpp
@@ -3,34 +3,35 @@
 #include "ellipse.h"
 
 
-int main(){
-
-	//Shape
+int main()
+{
+    //Shape
     Shape shape1("shape1");
     std::cout << "shape count : " << Shape::m_count << std::endl;//1
-    
+
     Shape shape2("shape2");
     std::cout << "shape count : " << Shape::m_count << std::endl;//2
-    
+
     Shape shape3;
     std::cout << "shape count : " << Shape::m_count << std::endl; // 3
-    
-    std::cout  << "***********************************************" << std::endl;
-    
+
+    std::cout << "***********************************************" << std::endl;
+
     //Ellipse
-    Ellipse ellipse1(10,12,"ellipse1");
+    Ellipse ellipse1(10, 12, "ellipse1");
     std::cout << "shape count : " << Shape::m_count << std::endl;// 4
     std::cout << "ellipse count : " << Ellipse::m_count << std::endl;//1
 
 
-    std::cout  << "***********************************************" << std::endl;
+    std::cout << "***********************************************" << std::endl;
 
     //Shape polymorphism
 
-    Shape* shapes[] {&shape1,&ellipse1};
-    for(auto &s : shapes){
+    Shape* shapes[] { &shape1,&ellipse1 };
+    for (auto& s : shapes)
+    {
         std::cout << "count : " << s->get_count() << std::endl;
     }
-   
+
     return 0;
 }
