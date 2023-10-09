6fd4fed10ae6010b7ce9f34460b3b65c700669f6
Arturs <xyzdelete@protonmail.com>
2023-10-09 13:14:04 +0300

    413. Dynamic binding with virtual functions

--- 37.Polymorphism/37.3PolymorphismWithVirtualFunctions/main.cpp
+++ 37.Polymorphism/37.3PolymorphismWithVirtualFunctions/main.cpp
@@ -3,30 +3,33 @@
 #include "oval.h"
 #include "circle.h"
 
-void draw_shape(Shape * s){
+void draw_shape(Shape* s)
+{
     s->draw();
 }
 
-void draw_shape_v1(const Shape& s_r){
+void draw_shape_v1(const Shape& s_r)
+{
     s_r.draw();
 }
 
 
-int main(){
+int main()
+{
 
     Shape shape1("Shape1");
     //shape1.draw();
 
-    Oval oval1(2.0,3.5,"Oval1");
+    Oval oval1(2.0, 3.5, "Oval1");
     //oval1.draw();
 
-    Circle circle1(3.3,"Circle1");
+    Circle circle1(3.3, "Circle1");
     //circle1.draw();
 
 
 
     //Base pointers
-    Shape * shape_ptr = &shape1;
+    Shape* shape_ptr = &shape1;
     //shape_ptr->draw(); // Shape::draw
 
     shape_ptr = &oval1;
@@ -41,26 +44,27 @@ int main(){
     //shape_ref.draw(); // Cicle::draw()
 
 
- 
+
     //Drawing shapes
     //draw_shape(&circle1);
     //draw_shape_v1(circle1);
 
 
     //Raw pointers
-    shape_ptr  = &oval1;
+    shape_ptr = &oval1;
     //shape_ptr->get_x_rad();
 
 
     //Shapes stored in collections
-    Shape* shape_collection[]{&shape1,&oval1,&circle1};
-	 
-	 for(Shape* s_ptr : shape_collection){
+    Shape* shape_collection[] { &shape1,&oval1,&circle1 };
+
+    for (Shape* s_ptr : shape_collection)
+    {
         s_ptr->draw();
     }
-  
 
-    
-   
+
+
+
     return 0;
 }
