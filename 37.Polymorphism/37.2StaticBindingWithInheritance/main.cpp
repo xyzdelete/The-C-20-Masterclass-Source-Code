ea0b699af6c8c52fd6b886c2b45ac578c605cec4
Arturs <xyzdelete@protonmail.com>
2023-10-09 12:38:01 +0300

    412. Static Binding with Inheritance

--- 37.Polymorphism/37.2StaticBindingWithInheritance/main.cpp
+++ 37.Polymorphism/37.2StaticBindingWithInheritance/main.cpp
@@ -3,30 +3,32 @@
 #include "oval.h"
 #include "circle.h"
 
-void draw_circle(const Circle& circle){
+void draw_circle(const Circle& circle)
+{
     circle.draw();
 }
 
-void draw_oval(const Oval& oval){
+void draw_oval(const Oval& oval)
+{
     oval.draw();
 }
 // More functions as you deal with more shape types. 30 ? 70? 100? It's messy.
 
 
 
-int main(){
-
+int main()
+{
     Shape shape1("Shape1");
     //shape1.draw();
 
-    Oval oval1(2.0,3.5,"Oval1");
+    Oval oval1(2.0, 3.5, "Oval1");
     //oval1.draw();
 
-    Circle circle1(3.3,"Circle1");
+    Circle circle1(3.3, "Circle1");
     //circle1.draw();
 
     //Base pointers
-    Shape * shape_ptr = &shape1;
+    Shape* shape_ptr = &shape1;
     //shape_ptr->draw();
 
     shape_ptr = &oval1;
@@ -48,11 +50,11 @@ int main(){
 
 
     //Shapes stored in collections
-     Circle circle_collection[]{circle1,Circle(10.0,"Circle2"),Circle(20.0,"Circle3")};
-     Oval oval_collection[]{oval1,Oval(22.3,51.1,"Oval2")};
-     //More arrays as you have more shape types, 100? Messy right?
+    Circle circle_collection[] { circle1,Circle(10.0,"Circle2"),Circle(20.0,"Circle3") };
+    Oval oval_collection[] { oval1,Oval(22.3,51.1,"Oval2") };
+    //More arrays as you have more shape types, 100? Messy right?
 
-    
-   
+
+
     return 0;
 }
