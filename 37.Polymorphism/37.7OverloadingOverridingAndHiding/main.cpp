8a903cc59ef6af6b5861f132c00bf6c059e8a795
Arturs <xyzdelete@protonmail.com>
2023-10-09 17:25:07 +0300

    417. Overloading, overriding and function hiding

--- 37.Polymorphism/37.7OverloadingOverridingAndHiding/main.cpp
+++ 37.Polymorphism/37.7OverloadingOverridingAndHiding/main.cpp
@@ -4,10 +4,10 @@
 #include "oval.h"
 #include "circle.h"
 
-int main(){
-
-    Shape * shape_ptr = new Circle(10,"Circle1");
-    shape_ptr->draw(45,"Red");
+int main()
+{
+    Shape* shape_ptr = new Circle(10, "Circle1");
+    shape_ptr->draw(45, "Red");
 
     return 0;
 }
