0140bbc8f84f9128e701ed142e052d273aa72e55
Arturs <xyzdelete@protonmail.com>
2023-10-10 12:24:21 +0300

    429. Pure virtual functions and abstract classes

--- 37.Polymorphism/37.19PureVirtualFunctionsAndAbstractClasses/main.cpp
+++ 37.Polymorphism/37.19PureVirtualFunctionsAndAbstractClasses/main.cpp
@@ -2,11 +2,11 @@
 #include "circle.h"
 #include "rectangle.h"
 
-int main(){
-   
-   //Shape * shape_ptr = new Shape; // Compiler error
+int main()
+{
+    //Shape * shape_ptr = new Shape; // Compiler error
 
-    const Shape * shape_rect = new Rectangle(10,10,"rect1");
+    const Shape* shape_rect = new Rectangle(10, 10, "rect1");
     double surface = shape_rect->surface();
     std::cout << "dynamic type of shape_rect : " << typeid(*shape_rect).name() << std::endl;
     std::cout << "The surface of shape rect is : " << surface << std::endl;
@@ -14,7 +14,7 @@ int main(){
 
     std::cout << "--------------" << std::endl;
 
-    const Shape * shape_circle = new Circle(10,"circle1");
+    const Shape* shape_circle = new Circle(10, "circle1");
     surface = shape_circle->surface();
     std::cout << "dynamic type of shape_circle : " << typeid(*shape_circle).name() << std::endl;
     std::cout << "The surface of the circle is : " << surface << std::endl;
