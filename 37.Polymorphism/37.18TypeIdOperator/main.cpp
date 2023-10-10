bd3f5d93df9e8894aaa7d15d7d204d54ed979593
Arturs <xyzdelete@protonmail.com>
2023-10-10 11:58:32 +0300

    428. typeid() operator

--- 37.Polymorphism/37.18TypeIdOperator/main.cpp
+++ 37.Polymorphism/37.18TypeIdOperator/main.cpp
@@ -2,14 +2,17 @@
 #include "polymorphic.h"
 #include "nonpolymorphic.h"
 
-
-int main(){
+int main()
+{
 
 	//1. typeid with fundemental types : returns static type
-    std::cout << "typeid(int) : " << typeid(int).name() << std::endl;
-	if(typeid(22.2f) == typeid(float)){
+	std::cout << "typeid(int) : " << typeid(int).name() << std::endl;
+	if (typeid(22.2f) == typeid(float))
+	{
 		std::cout << "22.2f is a float" << std::endl;
-	}else{
+	}
+	else
+	{
 		std::cout << "22.2f is not float" << std::endl;
 	}
 
@@ -26,10 +29,10 @@ int main(){
 
 	//3. typeid with pointers(polymorphic)
 	std::cout << "Polymorphic pointers : " << std::endl;
-	DynamicBase * b_ptr = new DynamicDerived;
-  
+	DynamicBase* b_ptr = new DynamicDerived;
+
 	std::cout << "Type of b_ptr : " << typeid(b_ptr).name() << std::endl; // static type
-	
+
 	//ATTENTION :
 	//			 For pointers you have to dereference to see the dynamic type //
 	std::cout << "Type of *b_ptr : " << typeid(*b_ptr).name() << std::endl;
@@ -40,17 +43,17 @@ int main(){
 	//4. type id with non polymorphic pointers and refs : We'll get static types
 	//because we're using static binding, the default behavior
 	std::cout << "Non polymorphic pointers and refs : " << std::endl;
-	StaticBase * b_ptr_s = new StaticDerived;
+	StaticBase* b_ptr_s = new StaticDerived;
 	StaticDerived staticderived;
-	StaticBase& static_base_ref {staticderived};
-  
+	StaticBase& static_base_ref { staticderived };
+
 	std::cout << "Type of *b_ptr_s : " << typeid(*b_ptr_s).name() << std::endl;
 	std::cout << "Type of static_base_ref : " << typeid(static_base_ref).name() << std::endl;
-  
 
-    delete b_ptr;
-    delete b_ptr_s;
-	
 
-    return 0;
+	delete b_ptr;
+	delete b_ptr_s;
+
+
+	return 0;
 }
