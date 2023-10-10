6b1b40081acb8a37ad716798d07f536b6ba0cfa3
Arturs <xyzdelete@protonmail.com>
2023-10-10 10:24:58 +0300

    424. Virtual functions with default arguments

--- 37.Polymorphism/37.14VirtualFunctionsWithDefaultArguments/main.cpp
+++ 37.Polymorphism/37.14VirtualFunctionsWithDefaultArguments/main.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include "derived.h"
 
-int main(){
-
+int main()
+{
     //Base ptr : Uses polymorphism
-    Base * base_ptr1 = new Derived;
+    Base* base_ptr1 = new Derived;
     double result = base_ptr1->add();
-    std::cout <<"Result (base ptr) : " << result  << std::endl; //12
+    std::cout << "Result (base ptr) : " << result << std::endl; //12
 
 
-    std::cout << "---------------------"<< std::endl;
-	
+    std::cout << "---------------------" << std::endl;
+
     //Base ref : Uses Polymorphism
     Derived derived1;
     Base& base_ref1 = derived1;
     result = base_ref1.add();
     std::cout << "Result (base ref) : " << result << std::endl; // 12
-    
-    std::cout << "---------------------"<< std::endl;
+
+    std::cout << "---------------------" << std::endl;
 
 
     //Raw objects
@@ -25,20 +25,20 @@ int main(){
     result = base3.add();
     std::cout << "raw result : " << result << std::endl;
 
-    std::cout << "---------------------"<< std::endl;
+    std::cout << "---------------------" << std::endl;
 
     //Direct object : Uses static binding
     Derived derived2;
     result = derived2.add();
     std::cout << "Result (Direct object) : " << result << std::endl; // 22
-	
-    std::cout << "---------------------"<< std::endl;
 
-	//Raw objects - slicing : uses static binding
-	Base base1 = derived2;
-	result = base1.add();
+    std::cout << "---------------------" << std::endl;
+
+    //Raw objects - slicing : uses static binding
+    Base base1 = derived2;
+    result = base1.add();
     std::cout << "Result (Raw objects assignment) : " << result << std::endl; //11
 
-   
+
     return 0;
 }
