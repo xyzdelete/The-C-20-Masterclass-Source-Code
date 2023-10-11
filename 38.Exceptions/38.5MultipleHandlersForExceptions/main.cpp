92b681388c6d6eca12eadce97aee0c0449456dfb
Arturs <xyzdelete@protonmail.com>
2023-10-11 12:42:40 +0300

    436. Multiple Handlers for an Exception

--- 38.Exceptions/38.5MultipleHandlersForExceptions/main.cpp
+++ 38.Exceptions/38.5MultipleHandlersForExceptions/main.cpp
@@ -5,53 +5,70 @@ void f1();
 void f2();
 void f3();
 
-void exception_thrower(){
+void exception_thrower()
+{
     std::cout << "starting exception_thrower()" << std::endl;
     throw 0;// Execution will half from here
     std::cout << "ending exception_thrower()" << std::endl;
 }
 
 
-int main(){
+int main()
+{
 
-    try{
+    try
+    {
         f1();
-    }catch(int ex){
-        std::cout << "Handling execution in main()" << std::endl;        
+    }
+    catch (int ex)
+    {
+        std::cout << "Handling execution in main()" << std::endl;
     }
     std::cout << "main() finishing up" << std::endl;
-   
+
     return 0;
 }
 
 //Definitions
 
-void f1(){
+void f1()
+{
     std::cout << "Starting f1()" << std::endl;
-    try{
+    try
+    {
         f2();
-    }catch(int ex){
+    }
+    catch (int ex)
+    {
         std::cout << "Exception handled in f1()" << std::endl;
     }
     std::cout << "Ending f1()" << std::endl;
 }
 
-void f2(){
+void f2()
+{
     std::cout << "Starting f2()" << std::endl;
-    try{
+    try
+    {
         f3();
-    }catch(int ex){
+    }
+    catch (int ex)
+    {
         std::cout << "Exception handled in f2()" << std::endl;
     }
-    std::cout << "Ending f2()" << std::endl;    
+    std::cout << "Ending f2()" << std::endl;
 }
 
-void f3(){
+void f3()
+{
     std::cout << "Starting f3()" << std::endl;
-    try{
+    try
+    {
         exception_thrower();
-    }catch(int ex){
+    }
+    catch (int ex)
+    {
         std::cout << "Exception handled in f3()" << std::endl;
     }
-    std::cout << "Ending f3()" << std::endl;    
+    std::cout << "Ending f3()" << std::endl;
 }
