17fd861914f33ff4f83e4988d2abb4180fe5b37e
Arturs <xyzdelete@protonmail.com>
2023-10-11 11:20:41 +0300

    435. Handling Exceptions At Different Levels

--- 38.Exceptions/38.4HandlingExceptionsAtDifferentLevels/main.cpp
+++ 38.Exceptions/38.4HandlingExceptionsAtDifferentLevels/main.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 
-
 //Declarations
 void f1();
 void f2();
 void f3();
 
-void exception_thrower(){
+void exception_thrower()
+{
     std::cout << "starting execution_thrower()" << std::endl;
-    try{
+    try
+    {
         throw 0;// Execution will halt from here
-    }catch(int ex){
+    }
+    catch (int ex)
+    {
         std::cout << "Something went wrong" << std::endl;
     }
     std::cout << "ending execution_thrower()" << std::endl;
 }
 
-int main(){
+int main()
+{
 
     f1();
 
@@ -24,12 +28,12 @@ int main(){
     try{
         f1();
     }catch(int ex){
-        std::cout << "Handling execution in main()" << std::endl;        
+        std::cout << "Handling execution in main()" << std::endl;
     }
     */
 
     std::cout << "END." << std::endl;
-   
+
     return 0;
 }
 
@@ -46,12 +50,12 @@ void f1(){
 void f2(){
     std::cout << "Starting f2()" << std::endl;
     f3();
-    std::cout << "Ending f2()" << std::endl;    
+    std::cout << "Ending f2()" << std::endl;
 }
 
 void f3(){
     std::cout << "Starting f3()" << std::endl;
-    std::cout << "Ending f3()" << std::endl;    
+    std::cout << "Ending f3()" << std::endl;
 }
 */
 
@@ -67,13 +71,13 @@ void f1(){
 void f2(){
     std::cout << "Starting f2()" << std::endl;
     f3();
-    std::cout << "Ending f2()" << std::endl;    
+    std::cout << "Ending f2()" << std::endl;
 }
 
 void f3(){
     std::cout << "Starting f3()" << std::endl;
     exception_thrower();
-    std::cout << "Ending f3()" << std::endl;    
+    std::cout << "Ending f3()" << std::endl;
 }
 
 */
@@ -90,7 +94,7 @@ void f1(){
 void f2(){
     std::cout << "Starting f2()" << std::endl;
     f3();
-    std::cout << "Ending f2()" << std::endl;    
+    std::cout << "Ending f2()" << std::endl;
 }
 
 void f3(){
@@ -100,7 +104,7 @@ void f3(){
     }catch(int ex){
         std::cout << "Handling execution in f3()" << std::endl;
     }
-    std::cout << "Ending f3()" << std::endl;    
+    std::cout << "Ending f3()" << std::endl;
 }
 */
 
@@ -120,15 +124,15 @@ void f2(){
     try{
         f3();
     }catch(int ex){
-        std::cout << "Handling execution in f2()" << std::endl;        
+        std::cout << "Handling execution in f2()" << std::endl;
     }
-    std::cout << "Ending f2()" << std::endl;    
+    std::cout << "Ending f2()" << std::endl;
 }
 
 void f3(){
     std::cout << "Starting f3()" << std::endl;
     exception_thrower();
-    std::cout << "Ending f3()" << std::endl;    
+    std::cout << "Ending f3()" << std::endl;
 }
 */
 
@@ -141,7 +145,7 @@ void f1(){
     try{
         f2();
     }catch(int ex){
-        std::cout << "Handling execution in f1()" << std::endl;        
+        std::cout << "Handling execution in f1()" << std::endl;
     }
     std::cout << "Ending f1()" << std::endl;
 }
@@ -149,13 +153,13 @@ void f1(){
 void f2(){
     std::cout << "Starting f2()" << std::endl;
     f3();
-    std::cout << "Ending f2()" << std::endl;    
+    std::cout << "Ending f2()" << std::endl;
 }
 
 void f3(){
     std::cout << "Starting f3()" << std::endl;
     exception_thrower();
-    std::cout << "Ending f3()" << std::endl;    
+    std::cout << "Ending f3()" << std::endl;
 }
 */
 
@@ -165,22 +169,25 @@ void f3(){
 //Exception thrown from f3() scope : Handled in main
 
 
-void f1(){
+void f1()
+{
     std::cout << "Starting f1()" << std::endl;
     f2();
     std::cout << "Ending f1()" << std::endl;
 }
 
-void f2(){
+void f2()
+{
     std::cout << "Starting f2()" << std::endl;
     f3();
-    std::cout << "Ending f2()" << std::endl;    
+    std::cout << "Ending f2()" << std::endl;
 }
 
-void f3(){
+void f3()
+{
     std::cout << "Starting f3()" << std::endl;
     exception_thrower();
-    std::cout << "Ending f3()" << std::endl;    
+    std::cout << "Ending f3()" << std::endl;
 }
 
 
