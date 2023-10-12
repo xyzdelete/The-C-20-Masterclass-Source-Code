db16290947a7323379ed9cc0fb318272302d3a1c
Arturs <xyzdelete@protonmail.com>
2023-10-12 10:23:37 +0300

    443. Ellipsis catch all block

--- 38.Exceptions/38.12EllipsisCatchAllBlock/main.cpp
+++ 38.Exceptions/38.12EllipsisCatchAllBlock/main.cpp
@@ -1,42 +1,51 @@
 #include <iostream>
 #include "exceptions.h"
 
-void some_function(size_t i){
-    if(i == 0)
+void some_function(size_t i)
+{
+    if (i == 0)
         throw 1;
-    if(i ==2)
+    if (i == 2)
         throw "const char*-Hello there";
-    if(i==3)
+    if (i == 3)
         throw CriticalError("Something is wrong");
-    if(i==4)
-        throw std::string("std::string-Hello there");     
+    if (i == 4)
+        throw std::string("std::string-Hello there");
 }
 
 
-int main(){
+int main()
+{
 
-   try{
-        for( size_t i{}; i < 5; ++i){
-            try{
+    try
+    {
+        for (size_t i {}; i < 5; ++i)
+        {
+            try
+            {
                 some_function(i);
             }
-            catch(int ex){
+            catch (int ex)
+            {
                 std::cout << "int handler- Cought an integer" << std::endl;
             }
-            catch(...){
+            catch (...)
+            {
                 std::cout << "Inner... handler , Cought some exception" << std::endl;
                 throw;
             }
-        }        
+        }
     }
-    catch(const std::string& ex){
+    catch (const std::string& ex)
+    {
         std::cout << "Cought some string exception" << std::endl;
     }
-    catch(...){
+    catch (...)
+    {
         std::cout << "Outer ...handler cought some other exception" << std::endl;
     }
 
-  
-   
+
+
     return 0;
 }
