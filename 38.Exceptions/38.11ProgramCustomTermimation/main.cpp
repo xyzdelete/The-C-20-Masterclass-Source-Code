41c444dfdc73b93176d65d9e4dc858f96a461a52
Arturs <xyzdelete@protonmail.com>
2023-10-12 10:14:27 +0300

    442. Program custom termination

--- 38.Exceptions/38.11ProgramCustomTermimation/main.cpp
+++ 38.Exceptions/38.11ProgramCustomTermimation/main.cpp
@@ -3,25 +3,28 @@
 #include <chrono>
 #include <thread>
 
-void our_terminate_function(){
+void our_terminate_function()
+{
     std::cout << "Our custom terminate function called" << std::endl;
-	std::cout << "Program will terminate in 10s ..." << std::endl;
+    std::cout << "Program will terminate in 10s ..." << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(10000));//Wait 10 more seconds
     std::abort();
 }
 
 
-int main(){
+int main()
+{
 
     //std::set_terminate(&our_terminate_function);
-    std::set_terminate([](){
-        std::cout << "Our custom terminate function called" << std::endl;
-        std::cout << "Program will terminate in 10s ..." << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(10000));//Wait 10 more seconds
-        std::abort();
+    std::set_terminate([]()
+{
+    std::cout << "Our custom terminate function called" << std::endl;
+    std::cout << "Program will terminate in 10s ..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));//Wait 10 more seconds
+    std::abort();
     });
 
     throw 1;
-   
+
     return 0;
 }
