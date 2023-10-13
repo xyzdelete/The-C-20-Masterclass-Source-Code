6320a63246a110873db48410ff39926ad8357934
Arturs <xyzdelete@protonmail.com>
2023-10-13 10:10:32 +0300

    452. Constructing and destructing

--- 39.Practice-BoxContainerType/39.2ConstructingDestroying/main.cpp
+++ 39.Practice-BoxContainerType/39.2ConstructingDestroying/main.cpp
@@ -2,17 +2,17 @@
 #include "boxcontainer.h"
 
 
-int main(){
-
+int main()
+{
 	BoxContainer box1(15);
 	box1.dummy_initialize();
-	
+
 	std::cout << "box1 : " << box1 << std::endl;
 	std::cout << "&box1 : " << &box1 << std::endl;
-	
+
 	BoxContainer box2(box1);
 	std::cout << "box2 : " << box2 << std::endl;
 	std::cout << "&box2 : " << &box2 << std::endl;
-   
-    return 0;
+
+	return 0;
 }
