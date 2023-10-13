bc2f711fe94560a3b8d7e3dc5f5268decd8ebd51
Arturs <xyzdelete@protonmail.com>
2023-10-13 11:18:49 +0300

    Stop 457. Storing In Different Types

--- 39.Practice-BoxContainerType/39.7StoringInDifferentTypes/main.cpp
+++ 39.Practice-BoxContainerType/39.7StoringInDifferentTypes/main.cpp
@@ -4,32 +4,33 @@
 #include "charcontainer.h"
 
 
-int main(){
+int main()
+{
 
 	DoubleContainer double_box1;
 	double_box1.add(11.2);
 	double_box1.add(33.5);
-	
+
 	std::cout << "double_box1 : " << double_box1 << std::endl;
-	
+
 	IntContainer int_box1;
 	int_box1.add(10);
 	int_box1.add(20);
-	
+
 	std::cout << "int_box1 : " << int_box1 << std::endl;
-	
+
 	CharContainer char_box1;
 	char_box1.add('H');
 	char_box1.add('e');
 	char_box1.add('l');
 	char_box1.add('l');
 	char_box1.add('o');
-	
+
 	std::cout << "char_box1 : " << char_box1 << std::endl;
 
-    char_box1.remove_all('l');
+	char_box1.remove_all('l');
 	std::cout << "char_box1 : " << char_box1 << std::endl;
-    
 
-    return 0;
+
+	return 0;
 }
