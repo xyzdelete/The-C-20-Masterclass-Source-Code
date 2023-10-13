71e9551e03bad13d3f155ca2144dc3b00bbd1884
Arturs <xyzdelete@protonmail.com>
2023-10-13 10:29:35 +0300

    453. Adding and Expanding

--- 39.Practice-BoxContainerType/39.3AddingItems/main.cpp
+++ 39.Practice-BoxContainerType/39.3AddingItems/main.cpp
@@ -2,8 +2,8 @@
 #include "boxcontainer.h"
 
 
-int main(){
-
+int main()
+{
 	BoxContainer box1(5);
 	std::cout << "box1 : " << box1 << std::endl;
 
@@ -11,22 +11,23 @@ int main(){
 	box1.add(12);
 	box1.add(13);
 	std::cout << "box1 : " << box1 << std::endl;
-	
+
 	box1.add(14);
 	box1.add(15);
 	std::cout << "box1 : " << box1 << std::endl;
-	
+
 	box1.add(16);
 	std::cout << "box1 : " << box1 << std::endl;
-	
-	for(size_t i{0}; i < 4 ; ++i){
-		box1.add(17+i);
+
+	for (size_t i { 0 }; i < 4 ; ++i)
+	{
+		box1.add(17 + i);
 	}
 	std::cout << "box1 : " << box1 << std::endl;
-	
-	box1.add(21);
-	std::cout << "box1 : "  << box1 << std::endl;
 
-   
-    return 0;
+	box1.add(21);
+	std::cout << "box1 : " << box1 << std::endl;
+
+
+	return 0;
 }
