162c99a97aed8800924f07d979b829d1f5d951a4
Arturs <xyzdelete@protonmail.com>
2023-10-17 10:17:11 +0300

    460. Your First Class Template

--- 40.ClassTemplates/40.2YourFirstClassTemplate/main.cpp
+++ 40.ClassTemplates/40.2YourFirstClassTemplate/main.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include "boxcontainer.h"
 
+class Point {
+	friend std::ostream& operator<<(std::ostream& out, const Point& operand);
+public:
+	Point(double x, double y) : m_x(x), m_y(y)
+	{
 
+	}
+	//The default constructor must be there for bags of Points to be able
+	//initializable with the new T[] statement in the BoxContainer<T> constructor.
+	Point() : m_x(0.0), m_y(0.0)
+	{
+	}
 
-class Point{
-    friend std::ostream& operator<<(std::ostream& out, const Point& operand);
-    public : 
-    Point(double x, double y) : m_x(x) , m_y(y){
-        
-    }
-    //The default constructor must be there for bags of Points to be able
-    //initializable with the new T[] statement in the BoxContainer<T> constructor.
-    Point() : m_x(0.0), m_y(0.0){  
-    }
-    
-private : 
-    double m_x{};
-    double m_y{};
+private:
+	double m_x {};
+	double m_y {};
 };
 
-std::ostream& operator<<(std::ostream& out, const Point& operand){
-    out << "Point [ x : " << operand.m_x << ",y : " << operand.m_y << "]";
-    return out;
+std::ostream& operator<<(std::ostream& out, const Point& operand)
+{
+	out << "Point [ x : " << operand.m_x << ",y : " << operand.m_y << "]";
+	return out;
 }
 
 
-int main(){
+int main()
+{
 
 	//int
 	std::cout << "BoxContainer of int : " << std::endl;
@@ -33,7 +35,7 @@ int main(){
 	int_box.add(33);
 	int_box.add(44);
 	std::cout << "int_box : " << int_box << std::endl;
-	
+
 	//double
 	std::cout << std::endl;
 	std::cout << "BoxContainer of double : " << std::endl;
@@ -41,15 +43,15 @@ int main(){
 	double_box.add(100.11);
 	double_box.add(200.22);
 	std::cout << "double_box : " << double_box << std::endl;
-	
+
 	//Point
 	std::cout << std::endl;
 	std::cout << "BoxContainer of Point : " << std::endl;
 	BoxContainer<Point> point_box;
-	point_box.add(Point(20.1,20.2));
-	point_box.add(Point(200.22,300.33));
+	point_box.add(Point(20.1, 20.2));
+	point_box.add(Point(200.22, 300.33));
 	std::cout << "point_box : " << point_box << std::endl;
-	
+
 	//std::string
 	std::cout << std::endl;
 	std::cout << "BoxContainer of std::string" << std::endl;
@@ -57,7 +59,7 @@ int main(){
 	string_box.add("Hello");
 	string_box.add("World");
 	std::cout << "string_box : " << string_box << std::endl;
-	
+
 	//char
 	std::cout << std::endl;
 	std::cout << "BoxContainer of char" << std::endl;
@@ -72,55 +74,55 @@ int main(){
 
 	//------------------------------------------------------
 
-    std::cout << std::endl;
+	std::cout << std::endl;
 	std::cout << "Trying out all the methods: " << std::endl;
-	
+
 	BoxContainer<int> int_box1;
 	int_box1.add(10);
 	int_box1.add(21);
 	int_box1.add(44);
-	
+
 	std::cout << "int_box1 : " << int_box1 << std::endl;
-	
+
 	int_box1.add(10);
 	int_box1.add(55);
 	int_box1.add(10);
 	std::cout << "int_box1 : " << int_box1 << std::endl;
-	
+
 	int_box1.remove_item(55);
 	std::cout << "int_box1 : " << int_box1 << std::endl;
-	
+
 	size_t removed = int_box1.remove_all(10);
-	std::cout << removed  << " items removed" << std::endl;
+	std::cout << removed << " items removed" << std::endl;
 	std::cout << "int_box1 : " << int_box1 << std::endl;
-	
+
 	//Copy construct
 	std::cout << std::endl;
 	std::cout << "Copy construction" << std::endl;
 	BoxContainer<int> int_box2(int_box);
 	std::cout << "int_box : " << int_box << std::endl;
 	std::cout << "int_box2 : " << int_box2 << std::endl;
-	
+
 	//Copy assign
 	std::cout << std::endl;
 	std::cout << "Copy assignment" << std::endl;
 	int_box2 = int_box1;
-	std::cout << "int_box1 : " << int_box1  << std::endl;
+	std::cout << "int_box1 : " << int_box1 << std::endl;
 	std::cout << "int_box2 : " << int_box2 << std::endl;
-	
+
 	std::cout << std::endl;
 	std::cout << "operator+= : " << std::endl;
-	int_box2 += int_box; 
-	std::cout << "int_box : " << int_box  << std::endl;
+	int_box2 += int_box;
+	std::cout << "int_box : " << int_box << std::endl;
 	std::cout << "int_box2 : " << int_box2 << std::endl;
-	
-	
+
+
 	std::cout << std::endl;
 	std::cout << "operator+" << std::endl;
 	BoxContainer<int> int_box3;
 	int_box3.add(100);
 	int_box3.add(200);
-	
+
 	BoxContainer<int> int_box4;
 	int_box4 = int_box2 + int_box3;
 	std::cout << "int_box4 : " << int_box4 << std::endl;
@@ -128,5 +130,5 @@ int main(){
 	std::cout << "END." << std::endl;
 
 
-    return 0;
+	return 0;
 }
