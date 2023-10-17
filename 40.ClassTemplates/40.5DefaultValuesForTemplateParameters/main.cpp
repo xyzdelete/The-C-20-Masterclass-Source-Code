fffee756dbd5c6804b42428877b2ce47a7df7330
Arturs <xyzdelete@protonmail.com>
2023-10-17 11:28:36 +0300

    463. Default Values for template parameters

--- 40.ClassTemplates/40.5DefaultValuesForTemplateParameters/main.cpp
+++ 40.ClassTemplates/40.5DefaultValuesForTemplateParameters/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include "boxcontainer.h"
 
-int main(){
-
-    /*
+int main()
+{
+	/*
 	BoxContainer int_box; // Defaults to <int,10>
 	int_box.add(10);
 	int_box.add(11);
@@ -17,16 +17,16 @@ int main(){
 	int_box.add(62);
 	std::cout << "int_box : " << int_box << std::endl;
 	int_box.add(55); // Maximum capacity is the default : 10
-    */
+	*/
 
-   /*
-	BoxContainer <double> double_box; // Defaults to <double,10>
-	double_box.add(11.1);
-    std::cout << "double box : " << double_box << std::endl;
-    */
+	/*
+	 BoxContainer <double> double_box; // Defaults to <double,10>
+	 double_box.add(11.1);
+	 std::cout << "double box : " << double_box << std::endl;
+	 */
 
 
-	BoxContainer <char,5> char_box; // Defaults are overriden
+	BoxContainer <char, 5> char_box; // Defaults are overriden
 	char_box.add('H');
 	char_box.add('e');
 	char_box.add('l');
@@ -35,5 +35,5 @@ int main(){
 	std::cout << "char_box : " << char_box << std::endl;
 	char_box.add('o');
 	std::cout << "char_box : " << char_box << std::endl;
-    return 0;
+	return 0;
 }
