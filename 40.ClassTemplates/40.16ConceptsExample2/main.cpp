8c18fff238b3cdd7a87ecfb351352eeb9f933b22
Arturs <xyzdelete@protonmail.com>
2023-10-18 12:28:46 +0300

    474. Concepts Example #2

--- 40.ClassTemplates/40.16ConceptsExample2/main.cpp
+++ 40.ClassTemplates/40.16ConceptsExample2/main.cpp
@@ -2,22 +2,24 @@
 #include <concepts>
 
 template <typename T>
-concept Number = (std::integral<T> || std::floating_point<T>) 
-                 && !std::same_as<T, bool>
-                 && !std::same_as<T, char>;
+concept Number = (std::integral<T> || std::floating_point<T>)
+&& !std::same_as<T, bool>
+&& !std::same_as<T, char>;
 
 
 template <Number T, Number  U>
-auto add ( T a, U b){
+auto add(T a, U b)
+{
     return a + b;
 }
 
 
-int main(){
+int main()
+{
 
     static_assert(Number<int>);
     //auto result = add(20.1, 20);
     //std::cout << "result :  " << result << std::endl;
-   
+
     return 0;
 }
