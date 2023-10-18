6735797b4c6d35283641d37383ecec44cf3ca9ad
Arturs <xyzdelete@protonmail.com>
2023-10-18 11:14:58 +0300

    471. Class templates with C++ 20 concepts

--- 40.ClassTemplates/40.13ClassTemplatesWithConcepts/main.cpp
+++ 40.ClassTemplates/40.13ClassTemplatesWithConcepts/main.cpp
@@ -3,54 +3,56 @@
 #include "boxcontainer.h"
 
 template <typename T>
-requires std::is_arithmetic_v<T>
-class Point{
+	requires std::is_arithmetic_v<T>
+class Point {
 	/*
-	    static_assert(std::is_arithmetic_v<T>,
-        "Coordinates of Point can only be numbers.");
+		static_assert(std::is_arithmetic_v<T>,
+		"Coordinates of Point can only be numbers.");
 		*/
-public : 
+public:
 	Point() = default;
-	Point(T x, T y)  
-	: m_x(x), m_y(y)
+	Point(T x, T y)
+		: m_x(x), m_y(y)
 	{
 	}
-	friend std::ostream& operator<< ( std::ostream& out, const Point<T> operand){
+	friend std::ostream& operator<< (std::ostream& out, const Point operand)
+	{
 		out << "Point [ x : " << operand.m_x
-					<< ", y : " << operand.m_y << "]";
+			<< ", y : " << operand.m_y << "]";
 		return out;
 	}
-private : 
+private:
 	T m_x;
 	T m_y;
 };
 
 
 
-int main(){
+int main()
+{
 
-	
-	Point<int> point_int(10,20);
+
+	Point<int> point_int(10, 20);
 	std::cout << "point_int : " << point_int << std::endl;
-	
-	Point<double> point_double(11.1,12.2);
+
+	Point<double> point_double(11.1, 12.2);
 	std::cout << "point_double : " << point_double << std::endl;
 
-    
+
 	//Point<std::string> point_string("Hello","World");
-    //std::cout << "point_string : " << point_string << std::endl;
+	//std::cout << "point_string : " << point_string << std::endl;
 
 
 
 	BoxContainer<Point<int>> point_box;
 	point_box.add(point_int);
-	point_box.add(Point<int>(40,50));
-	
+	point_box.add(Point<int>(40, 50));
+
 	std::cout << "point_box : " << point_box << std::endl;
 
 
 
-   
 
-    return 0;
+
+	return 0;
 }
