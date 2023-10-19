506684993ccda8ebfa0e6ae6c8cb466a25c08ebc
Arturs <xyzdelete@protonmail.com>
2023-10-19 12:38:26 +0300

    477. Lvalues and Rvalues

--- 41.MoveSemantics/41.2LvaluesAndRvalues/main.cpp
+++ 41.MoveSemantics/41.2LvaluesAndRvalues/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
-double add(double a , double b){
+double add(double a, double b)
+{
     return a + b;
 }
 
-int main(){
+int main()
+{
 
-    int x{5};	// x,y and z are all lvalues, they have a memory address we
-    int y{10};  //  can retrieve and use later on ,
-    int z{20};  // as long as the variables are in scope.
+    int x { 5 };	// x,y and z are all lvalues, they have a memory address we
+    int y { 10 };  //  can retrieve and use later on ,
+    int z { 20 };  // as long as the variables are in scope.
 
-    int * ptr = &x;
+    int* ptr = &x;
 
     z = (x + y) ;
     std::cout << "z : " << z << std::endl;
@@ -18,12 +20,12 @@ int main(){
 
     std::cout << "-------" << std::endl;
 
-    double result = add(10.1,20.2); // The result of add(10.1,20.2), is stored in some memory 
-                //location for a sh ort time, before it's assigned to result, and after it's copied
-                //into result, the memory is reclaimed by the system.
-                //add(10.1,20.2) is (evaluates to) an rvalue
-	
-	//std::cout << "address of add(10.1,20.2) : " << &(add(10.1,20.2)) << std::endl;//Error
+    double result = add(10.1, 20.2); // The result of add(10.1,20.2), is stored in some memory 
+    //location for a sh ort time, before it's assigned to result, and after it's copied
+    //into result, the memory is reclaimed by the system.
+    //add(10.1,20.2) is (evaluates to) an rvalue
+
+//std::cout << "address of add(10.1,20.2) : " << &(add(10.1,20.2)) << std::endl;//Error
     std::cout << "result is : " << result << std::endl;
 
 
@@ -36,6 +38,6 @@ int main(){
                                     // add(10.1 , 20.2) evaluates to an rvalue.
     //int* ptr3 = &45; // Compiler error.
     int* ptr4 = &x; //OK. x is an lvalue, so we can grab its address
-   
+
     return 0;
 }
