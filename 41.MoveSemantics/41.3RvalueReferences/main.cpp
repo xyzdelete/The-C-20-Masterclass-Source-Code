3349994c17878ef3e2d7278810ac1cf55f429308
Arturs <xyzdelete@protonmail.com>
2023-10-19 13:00:19 +0300

    478. Rvalue references

--- 41.MoveSemantics/41.3RvalueReferences/main.cpp
+++ 41.MoveSemantics/41.3RvalueReferences/main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 
-double add(double a , double b){
+double add(double a, double b)
+{
     return a + b;
 }
 
+int main()
+{
 
-int main(){
-    
-    int x{5};
-    int y{10};
+    int x { 5 };
+    int y { 10 };
 
     int&& result = x + y;
 
-    double&& outcome = add(10.1,20.2);
+    double&& outcome = add(10.1, 20.2);
 
 
     //
 
     std::cout << "result : " << result << std::endl;
     std::cout << "outcome : " << outcome << std::endl;
-   
+
     return 0;
 }
