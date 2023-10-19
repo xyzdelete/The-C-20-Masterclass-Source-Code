5173b7cfcfb5d4383bac931d6bd7a69eb59d41ab
Arturs <xyzdelete@protonmail.com>
2023-10-19 15:14:17 +0300

    482. Invalidating pointers after std::move

--- 41.MoveSemantics/41.7InvalidatePointersAfterStdMove/main.cpp
+++ 41.MoveSemantics/41.7InvalidatePointersAfterStdMove/main.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include "boxcontainer.h"
 
-void populate_box(BoxContainer<int>& box, int modifier){
-	for(size_t i{0} ; i < 20 ; ++i){
-		box.add((i+1)*modifier);
+void populate_box(BoxContainer<int>& box, int modifier)
+{
+	for (size_t i { 0 } ; i < 20 ; ++i)
+	{
+		box.add((i + 1) * modifier);
 	}
 }
 
+int main()
+{
+	BoxContainer<int> box1;
+	populate_box(box1, 2);
 
+	std::cout << "box1 : " << box1 << std::endl;
 
-int main(){
+	BoxContainer<int> box2(std::move(box1));
 
-    BoxContainer<int> box1;
-	populate_box(box1,2);
-
-    std::cout << "box1 : " << box1 << std::endl;
-    
-    BoxContainer<int> box2(std::move(box1));
-
-    std::cout << "box2 : " << box2 << std::endl;
-    std::cout << "box1 : " << box1 << std::endl;
+	std::cout << "box2 : " << box2 << std::endl;
+	std::cout << "box1 : " << box1 << std::endl;
 
 
 	std::cout << "------" << std::endl;
@@ -29,5 +29,5 @@ int main(){
 	std::cout << "box1 : " << box1 << std::endl;
 
 
-    return 0;
+	return 0;
 }
