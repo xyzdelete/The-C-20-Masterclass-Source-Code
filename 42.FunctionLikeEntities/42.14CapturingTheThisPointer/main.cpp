817c534ec80532f62ff4f981c9cf5d569f060cdb
Arturs <xyzdelete@protonmail.com>
2023-10-20 15:48:46 +0300

    499. Capturing the this pointer

--- 42.FunctionLikeEntities/42.14CapturingTheThisPointer/main.cpp
+++ 42.FunctionLikeEntities/42.14CapturingTheThisPointer/main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
-
-
-class Item{
-public : 
+class Item {
+public:
 
     Item(int a, int b)
-        : m_var1{a}, m_var2{b}
-    {}
-    void some_member_func(){
-		
-        auto func = [this](){
-            std::cout << "member vars :" << m_var1 << "," << m_var2 << std::endl;
-        };
+        : m_var1 { a }, m_var2 { b }
+    {
+    }
+    void some_member_func()
+    {
+
+        auto func = [this]()
+            {
+                std::cout << "member vars :" << m_var1 << "," << m_var2 << std::endl;
+            };
         func();
     };
-private : 
+private:
     int m_var1;
     int m_var2;
 };
 
 
-int main(){
+int main()
+{
 
-    Item item1(10,20);
+    Item item1(10, 20);
     item1.some_member_func();
-    
+
     return 0;
 }
