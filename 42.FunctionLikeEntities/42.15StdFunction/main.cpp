96af8ff4934b2d1afd85b9762a6c1af8d100757c
Arturs <xyzdelete@protonmail.com>
2023-10-20 17:37:56 +0300

    500. std::function

--- 42.FunctionLikeEntities/42.15StdFunction/main.cpp
+++ 42.FunctionLikeEntities/42.15StdFunction/main.cpp
@@ -2,9 +2,9 @@
 #include <functional>
 #include "boxcontainer.h"
 
-
 //Function pointer
-char encrypt(const char& param){
+char encrypt(const char& param)
+{
     return static_cast<char> (param + 3);
 }
 
@@ -12,8 +12,9 @@ char encrypt(const char& param){
 class Decrypt
 {
 public:
-    char operator()( const char& param){
-         return static_cast<char> (param - 3);
+    char operator()(const char& param)
+    {
+        return static_cast<char> (param - 3);
     }
 };
 
@@ -21,44 +22,52 @@ public:
 //Modifying a BoxContainer of strings
 BoxContainer<std::string>& modify(BoxContainer<std::string>& sentence,
                                         //char(*modifier) (const char&)){
-                                  std::function<char(const char&)> modifier){
-    for(size_t i{}; i < sentence.size() ; ++i){
-            
+                                  std::function<char(const char&)> modifier)
+{
+    for (size_t i {}; i < sentence.size() ; ++i)
+    {
+
         //Code below relies on get_item() to return a reference
         //Loop through the word modifying each character 
-        for(size_t j{} ; j < sentence.get_item(i).size(); ++j){
+        for (size_t j {} ; j < sentence.get_item(i).size(); ++j)
+        {
             sentence.get_item(i)[j] = modifier(sentence.get_item(i)[j]);
         }
     }
     return sentence;
 }
 
-std::string get_best (const BoxContainer<std::string>& sentence,
+std::string get_best(const BoxContainer<std::string>& sentence,
         // bool(*comparator)(const std::string& str1, const std::string& str2)){
-        std::function<bool(const std::string& str1,const std::string& str2)> comparator){
+        std::function<bool(const std::string& str1, const std::string& str2)> comparator)
+{
 
     std::string best = sentence.get_item(0);
-    for(size_t i{}; i < sentence.size() ; ++i){
-            
-        if(comparator(sentence.get_item(i),best)){
+    for (size_t i {}; i < sentence.size() ; ++i)
+    {
+
+        if (comparator(sentence.get_item(i), best))
+        {
             best = sentence.get_item(i);
         }
-            
+
     }
-        
+
     return best;
 }
 
 //Function pointer
-bool larger_in_size (const std::string& str1, const std::string& str2){
-    if(str1.size() > str2.size())
+bool larger_in_size(const std::string& str1, const std::string& str2)
+{
+    if (str1.size() > str2.size())
         return true;
-    else 
+    else
         return false;
 }
 
 
-int main(){
+int main()
+{
 
     //Basic usage of std::function : storing function like entities
     //in a unified way.
@@ -75,9 +84,10 @@ int main(){
 
 
     //Lambda function
-    my_modifier = [](const char& param){
-        return static_cast<char> (param + 3);
-    };
+    my_modifier = [](const char& param)
+        {
+            return static_cast<char> (param + 3);
+        };
     std::cout << "A encrypted becomes : " << my_modifier('A') << std::endl; // D
 
 
@@ -87,21 +97,23 @@ int main(){
     //Storing function like entities in a collection
     std::cout << std::endl;
     std::cout << "Collection of function like entities : " << std::endl;
-    
+
     BoxContainer<std::function<char(const char&)>> func_entities;
     func_entities.add(encrypt); // Function pointer
     func_entities.add(decrypt); // Functor
-    func_entities.add([](const char& param){ // Lambda function
-        return static_cast<char> (param + 3);
+    func_entities.add([](const char& param)
+{ // Lambda function
+    return static_cast<char> (param + 3);
     });
-    
-    for(size_t i{}; i < func_entities.size() ; ++i){
-        std::cout << "result " << i << ". D transformed becomes : " << 
-                func_entities.get_item(i)('D') << std::endl;
+
+    for (size_t i {}; i < func_entities.size() ; ++i)
+    {
+        std::cout << "result " << i << ". D transformed becomes : " <<
+            func_entities.get_item(i)('D') << std::endl;
     }
 
 
-    std::cout <<"--------" << std::endl;
+    std::cout << "--------" << std::endl;
 
     //std::function as callback 
     std::cout << std::endl;
@@ -113,27 +125,28 @@ int main(){
     quote.add("blue");
     quote.add("my");
     quote.add("friend");
-    std::cout << "Initial : " <<  quote << std::endl;
-    std::cout << "Encrypted : " << modify(quote,encrypt) << std::endl;
-    std::cout << "Decrypted : " << modify(quote,decrypt) << std::endl;
+    std::cout << "Initial : " << quote << std::endl;
+    std::cout << "Encrypted : " << modify(quote, encrypt) << std::endl;
+    std::cout << "Decrypted : " << modify(quote, decrypt) << std::endl;
 
 
     std::cout << "----------" << std::endl;
 
     std::cout << std::endl;
     std::cout << "Gettting the best : " << std::endl;
-    
-    auto  greater_lexicographically = [](const std::string& str1, const std::string& str2){
-    return (str1>str2);
-    };
-    
-    std::cout << "larger in size : " << get_best(quote,larger_in_size) << std::endl;
-    std::cout << "greater lexicographicaly : " 
-            << get_best(quote,greater_lexicographically) << std::endl;
+
+    auto  greater_lexicographically = [](const std::string& str1, const std::string& str2)
+        {
+            return (str1 > str2);
+        };
+
+    std::cout << "larger in size : " << get_best(quote, larger_in_size) << std::endl;
+    std::cout << "greater lexicographicaly : "
+        << get_best(quote, greater_lexicographically) << std::endl;
 
 
 
-  
-    
+
+
     return 0;
 }
