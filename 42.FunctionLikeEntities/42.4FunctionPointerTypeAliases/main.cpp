8b33ba072e90651d6bf1f03d030889f50c223d57
Arturs <xyzdelete@protonmail.com>
2023-10-20 12:20:10 +0300

    489. Function Pointer Type Aliases

--- 42.FunctionLikeEntities/42.4FunctionPointerTypeAliases/main.cpp
+++ 42.FunctionLikeEntities/42.4FunctionPointerTypeAliases/main.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 #include "boxcontainer.h"
 
-char encrypt(const char& param){ // Callback function
+char encrypt(const char& param)
+{ // Callback function
     return static_cast<char> (param + 3);
 }
 
-char decrypt(const char& param){ // Callback function
+char decrypt(const char& param)
+{ // Callback function
     return static_cast<char> (param - 3);
 }
 
 
-std::string & modify(std::string& str_param,
-                                        char(* modifier)(const char&))
+std::string& modify(std::string& str_param,
+                                        char(*modifier)(const char&))
 {
-     for(size_t i{} ; i < str_param.size() ; ++i){
+    for (size_t i {} ; i < str_param.size() ; ++i)
+    {
         str_param[i] = modifier(str_param[i]); // Calling the callback
-     }
-     return str_param;
+    }
+    return str_param;
 }
 
 //Modifying a BoxContainer of strings
 BoxContainer<std::string>& modify(BoxContainer<std::string>& sentence,
-                                        char(*modifier) (const char&)){
-    for(size_t i{}; i < sentence.size() ; ++i){
-            
+                                        char(*modifier) (const char&))
+{
+    for (size_t i {}; i < sentence.size() ; ++i)
+    {
+
         //Code below relies on get_item() to return a reference
         //Loop through the word modifying each character 
-        for(size_t j{} ; j < sentence.get_item(i).size(); ++j){
+        for (size_t j {} ; j < sentence.get_item(i).size(); ++j)
+        {
             sentence.get_item(i)[j] = modifier(sentence.get_item(i)[j]);
         }
     }
@@ -35,44 +41,50 @@ BoxContainer<std::string>& modify(BoxContainer<std::string>& sentence,
 
 
 //Using syntax
-using str_comparator = 
-    bool(*)(const std::string& str1, const std::string& str2);
+using str_comparator =
+bool(*)(const std::string& str1, const std::string& str2);
 
 //With typedefs
 //typedef bool(*str_comparator) (const std::string& str1, const std::string& str2);
 
-std::string get_best (const BoxContainer<std::string>& sentence,
-                        str_comparator comparator){
+std::string get_best(const BoxContainer<std::string>& sentence,
+                        str_comparator comparator)
+{
 
     std::string best = sentence.get_item(0);
-    for(size_t i{}; i < sentence.size() ; ++i){
-            
-        if(comparator(sentence.get_item(i),best)){
+    for (size_t i {}; i < sentence.size() ; ++i)
+    {
+
+        if (comparator(sentence.get_item(i), best))
+        {
             best = sentence.get_item(i);
         }
-            
+
     }
-         
+
     return best;
 }
 
 
-bool larger_in_size (const std::string& str1, const std::string& str2){
-    if(str1.size() > str2.size())
+bool larger_in_size(const std::string& str1, const std::string& str2)
+{
+    if (str1.size() > str2.size())
         return true;
-    else 
+    else
         return false;
 }
 
-bool greater_lexicographically(const std::string& str1, const std::string& str2){
-    return (str1>str2);
+bool greater_lexicographically(const std::string& str1, const std::string& str2)
+{
+    return (str1 > str2);
 }
 
 
-int main(){
+int main()
+{
 
-    std::string msg{"Hello"};
-    modify(msg,encrypt);
+    std::string msg { "Hello" };
+    modify(msg, encrypt);
     std::cout << "outcome : " << msg << std::endl;
 
     std::cout << "------" << std::endl;
@@ -84,20 +96,20 @@ int main(){
     quote.add("blue");
     quote.add("my");
     quote.add("friend");
-    std::cout << "Initial : " <<  quote << std::endl;
-    std::cout << "Encrypted : " << modify(quote,encrypt) << std::endl;
-    std::cout << "Decrypted : " << modify(quote,decrypt) << std::endl;
+    std::cout << "Initial : " << quote << std::endl;
+    std::cout << "Encrypted : " << modify(quote, encrypt) << std::endl;
+    std::cout << "Decrypted : " << modify(quote, decrypt) << std::endl;
 
 
     std::cout << "-------" << std::endl;
 
-    str_comparator callback{larger_in_size};
+    str_comparator callback { larger_in_size };
 
-    std::cout << "larger in size : " << get_best(quote,callback) << std::endl;
-    std::cout << "greater lexicographicaly : " 
-            << get_best(quote,greater_lexicographically) << std::endl;
+    std::cout << "larger in size : " << get_best(quote, callback) << std::endl;
+    std::cout << "greater lexicographicaly : "
+        << get_best(quote, greater_lexicographically) << std::endl;
 
 
-   
+
     return 0;
 }
