57bd00263524d36f5f0040deb7b11d727bb7bf05
Arturs <xyzdelete@protonmail.com>
2023-10-21 12:58:02 +0300

    508. Constant iterators

--- 43.StlContainersAndIterators/43.7ContantIterators/main.cpp
+++ 43.StlContainersAndIterators/43.7ContantIterators/main.cpp
@@ -3,12 +3,14 @@
 
 
 template <typename T>
-void print_collection(const T& collection){
-    
+void print_collection(const T& collection)
+{
+
     auto it = collection.begin();
-    
+
     std::cout << " [";
-    while(it != collection.end()){
+    while (it != collection.end())
+    {
         std::cout << " " << *it ;
         ++it;
     }
@@ -16,9 +18,10 @@ void print_collection(const T& collection){
 }
 
 
-int main(){
+int main()
+{
 
-    std::vector<int> numbers{ 11,22,33,44,55,66,77};
+    std::vector<int> numbers { 11,22,33,44,55,66,77 };
 
     std::cout << "numbers : ";
     print_collection(numbers);
@@ -38,8 +41,9 @@ int main(){
 
     //std::vector<int>::const_iterator c_it = numbers.cbegin();
     auto c_it = numbers.cbegin();
-    while( c_it != numbers.end()){
-       // *c_it = 100;
+    while (c_it != numbers.end())
+    {
+        // *c_it = 100;
         ++c_it;
     }
 
@@ -48,11 +52,12 @@ int main(){
     auto it1 = numbers.crbegin();
     //std::vector<int>::const_reverse_iterator it1= numbers.crbegin();
 
-    while(it1 != numbers.crend()){
-      //*it1 = 600; // Compiler error, it1 is a const iterator, we can't modify 
-                    // container data through it.
-      ++it1;
+    while (it1 != numbers.crend())
+    {
+        //*it1 = 600; // Compiler error, it1 is a const iterator, we can't modify 
+                      // container data through it.
+        ++it1;
     }
-   
+
     return 0;
 }
