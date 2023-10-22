8ad40ed333c118f7eac6305c4dc528514a6cfcd5
Arturs <xyzdelete@protonmail.com>
2023-10-22 21:17:32 +0300

    519. Associative Containers : Pair

--- 44.ZoomingOnSTLContainers/44.8Pair/main.cpp
+++ 44.ZoomingOnSTLContainers/44.8Pair/main.cpp
@@ -2,14 +2,13 @@
 #include <vector>
 #include <utility>
 
-
-int main(){
-
+int main()
+{
     //Code1 : Create pair
     std::cout << "creating pairs:" << std::endl;
-    std::pair<int,std::string> pair1{0,"Book shelf"};
-    
-    auto pair2 = std::make_pair(1,"Table"); // Deduces the template arguments
+    std::pair<int, std::string> pair1 { 0,"Book shelf" };
+
+    auto pair2 = std::make_pair(1, "Table"); // Deduces the template arguments
 
     //std::cout << "pair1 : " << pair1 << std::endl;
 
@@ -21,15 +20,15 @@ int main(){
     std::cout << "auto[] syntax - collect components of a pair into variables : " << std::endl;
     auto [int_var, string_var] = pair1;
     std::cout << "pair1 (auto[] syntax) : (" << int_var << "," << string_var << ")" << std::endl;
-    
-    
+
+
 
     //Code3 : template argument deduction (C++17)
     std::cout << std::endl;
     std::cout << "template argument deduction : " << std::endl;
-    
+
     std::pair student(33165, std::string("John Snow"));// Deducing the template arguments
-    
+
     std::cout << "student ID : " << student.first << std::endl;
     std::cout << "Student name : " << student.second << std::endl;
 
@@ -37,15 +36,16 @@ int main(){
     //Code4 : collection of pairs
     std::cout << std::endl;
     std::cout << "collection of pairs : " << std::endl;
-    
-    std::vector<std::pair<int,std::string>> collection {{10,"Earth"},{20,"Spins"},
+
+    std::vector<std::pair<int, std::string>> collection { {10,"Earth"},{20,"Spins"},
                                                         {30,"From"},{40,"Left"},
-                                                        {50,"To"},{60,"Right"}};
+                                                        {50,"To"},{60,"Right"} };
     //Traverse : syntax1
     std::cout << std::endl;
     std::cout << "syntax1 : " << std::endl;
     std::cout << "collection : [";
-    for(const auto& elt : collection){
+    for (const auto& elt : collection)
+    {
         std::cout << " (" << elt.first << "," << elt.second << ")";
     }
     std::cout << "]" << std::endl;
@@ -55,10 +55,11 @@ int main(){
     std::cout << std::endl;
     std::cout << "syntax2 : " << std::endl;
     std::cout << "collection : [";
-    for( auto[key,value] : collection){
+    for (auto [key, value] : collection)
+    {
         std::cout << " (" << key << "," << value << ")";
     }
     std::cout << "]" << std::endl;
-   
+
     return 0;
 }
