cb156dcde83d36851777ca153ed203e94c9c7248
Arturs <xyzdelete@protonmail.com>
2023-10-24 13:32:30 +0300

    544. Range Factories

--- 46.RangesLibraryInCpp20/46.7RangeFactories/main.cpp
+++ 46.RangesLibraryInCpp20/46.7RangeFactories/main.cpp
@@ -2,18 +2,19 @@
 #include <vector>
 #include <ranges>
 
-
-int main(){
+int main()
+{
 
     //Generate an infinite sequence of numbers
     //auto infinite_view = std::views::iota(1) | std::views::take(20); // Stores the compuation
     //auto data_view = std::views::take( std::views::iota(1) , 20);
 
     //Numbers are generated lazily, on the fly, as we need them in each iteration
-    for(auto i :std::views::iota(1) | std::views::take(20)){
+    for (auto i : std::views::iota(1) | std::views::take(20))
+    {
         std::cout << i << std::endl;
     }
-    
-  
+
+
     return 0;
 }
