58951f396f7ddde40c6b47b754ff840676438c5d
Arturs <xyzdelete@protonmail.com>
2023-10-25 13:55:21 +0300

    557. Wrapping Iterators From Other Containers

--- 47.BuildingIteratorsForCustomContainers/40.12WrappingIteratorsFromOtherContainers/main.cpp
+++ 47.BuildingIteratorsForCustomContainers/40.12WrappingIteratorsFromOtherContainers/main.cpp
@@ -3,40 +3,43 @@
 #include <algorithm>
 #include <ranges>
 
-
 template <typename T>
-class VectorWrapper{
-public: 
+class VectorWrapper {
+public:
     //Iterator methods
     std::vector<T>::iterator begin() { return m_items.begin(); }
-    std::vector<T>::iterator end()   { return m_items.end(); }
+    std::vector<T>::iterator end() { return m_items.end(); }
 
     //Const overloads
-    std::vector<T>::const_iterator begin() const{ return m_items.cbegin(); }
-    std::vector<T>::const_iterator end() const  { return m_items.cend(); }
+    std::vector<T>::const_iterator begin() const { return m_items.cbegin(); }
+    std::vector<T>::const_iterator end() const { return m_items.cend(); }
 
 
     std::vector<T>::const_iterator cbegin() { return m_items.cbegin(); }
-    std::vector<T>::const_iterator cend()   { return m_items.cend(); }
- 
-    friend std::ostream& operator<< (std::ostream& out, const VectorWrapper<T>& vec){
+    std::vector<T>::const_iterator cend() { return m_items.cend(); }
+
+    friend std::ostream& operator<< (std::ostream& out, const VectorWrapper<T>& vec)
+    {
         out << "Items : " ;
-        for (auto i : vec.m_items){
+        for (auto i : vec.m_items)
+        {
             out << i << " ";
         }
         return out;
-    }  
-    
-    void add( T item){
+    }
+
+    void add(T item)
+    {
         m_items.push_back(item);
     }
-private : 
+private:
     std::vector<T> m_items;
 };
 
-int main(){
+int main()
+{
 
-	VectorWrapper<std::string> greeting;
+    VectorWrapper<std::string> greeting;
     greeting.add("Hello");
     greeting.add("World!");
     greeting.add("How");
@@ -44,21 +47,23 @@ int main(){
     greeting.add("you");
     greeting.add("all");
     greeting.add("doing?");
-    
+
     std::cout << "greeting : " << greeting << std::endl;
 
     std::cout << "Range based for loop : " << std::endl;
-    for(auto i : greeting){
+    for (auto i : greeting)
+    {
         std::cout << i << " ";
     }
     std::cout << std::endl;
 
 
     std::cout << "taking only 2 : " << std::endl;
-    for(auto i : greeting | std::views::take(2)){
+    for (auto i : greeting | std::views::take(2))
+    {
         std::cout << i << " ";
     }
     std::cout << std::endl;
-   
+
     return 0;
 }
