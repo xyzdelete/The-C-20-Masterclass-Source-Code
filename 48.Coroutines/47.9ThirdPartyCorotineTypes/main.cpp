90077cc02bbe5a226f8c350421e298c8d509c224
Arturs <xyzdelete@protonmail.com>
2023-10-26 13:12:31 +0300

    567. Third Party Generator Type

--- 48.Coroutines/47.9ThirdPartyCorotineTypes/main.cpp
+++ 48.Coroutines/47.9ThirdPartyCorotineTypes/main.cpp
@@ -6,26 +6,26 @@
 /*
 template <typename T>
 struct generator {
-    struct promise_type { 
+    struct promise_type {
         T m_value;
-        generator get_return_object() { return generator(this); }    
-        std::suspend_always initial_suspend() { return {}; }        
+        generator get_return_object() { return generator(this); }
+        std::suspend_always initial_suspend() { return {}; }
         std::suspend_always final_suspend() noexcept{ return {}; }
-      
+
         void unhandled_exception() noexcept
         {
             std::rethrow_exception(std::current_exception());
         }
         std::suspend_always yield_value(T val) {
-            m_value = val; 
+            m_value = val;
             return {};
         }
-      
+
         void return_void() {
             std::cout << "Returning void..." << std::endl;
         }
-       
-       
+
+
     };
     generator(promise_type* p)
          : m_handle(std::coroutine_handle<promise_type>::from_promise(*p)) {}
@@ -67,7 +67,7 @@ unique_generator<int> infinite_number_stream(int start = 0)
     auto value = start;
     for (int i = 0;; ++i)
     {
-       // std::cout << "In infinite_number stream..." << std::endl;
+        // std::cout << "In infinite_number stream..." << std::endl;
         co_yield value;
         ++value;
     }
@@ -76,17 +76,19 @@ unique_generator<int> infinite_number_stream(int start = 0)
 
 unique_generator<int> range(int first, int last)
 {
-    while (first != last) {
+    while (first != last)
+    {
         co_yield first++;
     }
 }
 
 
 
-int main(){
+int main()
+{
 
 
-    
+
     auto task1 = generate_numbers();
 
     /*
@@ -94,30 +96,31 @@ int main(){
         std::cout << "value : " << *i << std::endl;
     }
     */
-   //std::cout << "value : " << task1() << std::endl;
-   
+    //std::cout << "value : " << task1() << std::endl;
 
-    /*
-   auto task2 = infinite_number_stream();
-   for(size_t i{}; ; ++i){
-    std::cout << "value : " << task2() << std::endl;
-   }
-   */
- 
+
+     /*
+    auto task2 = infinite_number_stream();
+    for(size_t i{}; ; ++i){
+     std::cout << "value : " << task2() << std::endl;
+    }
+    */
+
 
 
     //Range 
-    auto task3 = range(0,25);
+    auto task3 = range(0, 25);
     /*
     for(size_t i{}; i < 26; ++i ){
         std::cout << "value[" << i << "] : " << task3() << std::endl;
     }
     */
 
-    for(auto v : task3){
+    for (auto v : task3)
+    {
         std::cout << "value : " << v << std::endl;
     }
-  
+
 
 
     std::cout << "Done!" << std::endl;
