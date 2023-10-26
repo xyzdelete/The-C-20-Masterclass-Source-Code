f2cce892aab0c2dbdf12dceb499769139a0aaec6
Arturs <xyzdelete@protonmail.com>
2023-10-26 11:25:18 +0300

    563. co_await

--- 48.Coroutines/47.5CoAwait/main.cpp
+++ 48.Coroutines/47.5CoAwait/main.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
 #include <coroutine>
 
-
 struct CoroType {
-    struct promise_type { 
-        CoroType get_return_object() { return CoroType(this); }    
-        std::suspend_always initial_suspend() { return {}; }        
-        std::suspend_always final_suspend() noexcept{ return {}; }
-      
+    struct promise_type {
+        CoroType get_return_object() { return CoroType(this); }
+        std::suspend_always initial_suspend() { return {}; }
+        std::suspend_always final_suspend() noexcept { return {}; }
+
         void unhandled_exception() noexcept
         {
             std::rethrow_exception(std::current_exception());
         }
-        void return_void(){};
-       
+        void return_void() {};
+
     };
     CoroType(promise_type* p)
-         : m_handle(std::coroutine_handle<promise_type>::from_promise(*p)) {}
+        : m_handle(std::coroutine_handle<promise_type>::from_promise(*p))
+    {
+    }
     ~CoroType()
-     {
-         std::cout << "Handle destroyed..." << std::endl;
-          m_handle.destroy();
-     }
+    {
+        std::cout << "Handle destroyed..." << std::endl;
+        m_handle.destroy();
+    }
     std::coroutine_handle<promise_type>   m_handle;
 };
 
 
-CoroType do_work() {
+CoroType do_work()
+{
     std::cout << "Doing first thing... " << std::endl;
-    co_await std::suspend_always{};
+    co_await std::suspend_always {};
     std::cout << "Doing second thing..." << std::endl;
-    co_await std::suspend_always{};
+    co_await std::suspend_always {};
     std::cout << "Doing Third thing..." << std::endl;
 }
 
-int main(){
+int main()
+{
 
     auto task = do_work();
 
