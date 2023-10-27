4f4fb7f325f59f2826cf857c212760e093397919
Arturs <xyzdelete@protonmail.com>
2023-10-27 12:25:27 +0300

    576. Export Import

--- 49.Modules/48.8ExportImport/ExportImport/main.cpp
+++ 49.Modules/48.8ExportImport/ExportImport/main.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+
 import math_stuff;
 import print;
 
@@ -18,8 +19,9 @@ int main()
     std::cout << "point1 : " << point1 << std::endl;
 
 
-    std::vector<int> data{ 1,2,3,4,5 };
-    for (auto v : data) {
+    std::vector<int> data { 1,2,3,4,5 };
+    for (auto v : data)
+    {
         std::cout << "value : " << v << std::endl;
     }
 }
